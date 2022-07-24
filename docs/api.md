033bcf132db9700afa5331e526eab3613ccfac82
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-24 17:24:07 +0200

    🎉 Initial commit

---
+++ docs/api.md
@@ -0,0 +1,5 @@
+# API
+
+```{eval-rst}
+.. automodule:: lndb_cli
+```
---
+++ noxfile.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.run("pre-commit", "install")
+    session.run("pre-commit", "run", "--all-files")
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+def build(session):
+    session.install(".[dev,test]")
+    session.run(
+        "pytest",
+        "-s",
+        "--cov=lndb_cli",
+        "--cov-append",
+        "--cov-report=term-missing",
+    )
+    session.run("coverage", "xml")
+    prefix = "." if Path("./lndocs").exists() else ".."
+    session.install(f"{prefix}/lndocs")
+    session.run("lndocs")
