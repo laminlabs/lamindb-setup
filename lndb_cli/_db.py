a6d98cb4852337df3e1eee9d072b37fb74a0a773
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-25 18:39:00 +0200

    :green_heart: Fix

--- lndb_cli/_db.py
+++ lndb_cli/_db.py
@@ -2,7 +2,6 @@ import lamindb_schema as schema
 import sqlmodel as sqm
 
 from . import load_or_create_instance_settings
-from ._load import load
 
 
 class insert_if_not_exists:
@@ -12,13 +11,9 @@ class insert_if_not_exists:
     def user(cls, user_email, user_id):
         settings = load_or_create_instance_settings()
         engine = settings.db_engine()
-        with engine.connect() as conn:
-            statement = sqm.select(schema.core.user).where(
-                schema.core.user.id == user_id
-            )  # noqa
-            results = conn.exec(statement)  # noqa
-        df_user = load("user")
-        if user_id not in df_user.index:
+        with sqm.Session(engine) as session:
+            user = session.get(schema.core.user, user_id)
+        if user is None:
             user_id = insert.user(user_email, user_id)  # type: ignore
         return user_id
 
