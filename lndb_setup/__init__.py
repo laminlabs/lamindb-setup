9bb96e7b762835dc865e7fb9f097bb2754646c4e
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-26 16:43:12 +0200

    🚚 Make storage public (#6)
    :truck: Make storage public

--- lndb_setup/__init__.py
+++ lndb_setup/__init__.py
@@ -34,13 +34,14 @@ Dev API
 
    UserSettings
    InstanceSettings
+   Storage
 """
 
 __version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1a1
 from . import _check_versions  # noqa
 from ._schema import schema  # noqa
 from ._settings import settings  # noqa
-from ._settings_instance import InstanceSettings  # noqa
+from ._settings_instance import InstanceSettings, Storage  # noqa
 from ._settings_user import UserSettings  # noqa
 from ._setup_instance import init_instance, load_instance  # noqa
 from ._setup_user import log_in_user, sign_up_user  # noqa
