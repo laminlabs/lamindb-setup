3df831d1f32bf36efe92e799b9d74b2dd267e9d7
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-26 15:08:42 +0200

    :children_crossing: Expose functionality through one settings class

--- lndb_setup/__init__.py
+++ lndb_setup/__init__.py
@@ -3,6 +3,13 @@
 User API
 --------
 
+Settings:
+
+.. autosummary::
+   :toctree:
+
+   settings
+
 Setup user account (`lndb signup`, `lndb login`):
 
 .. autosummary::
@@ -32,11 +39,8 @@ Dev API
 __version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1a1
 from . import _check_versions  # noqa
 from ._schema import schema  # noqa
+from ._settings import settings  # noqa
 from ._settings_instance import InstanceSettings  # noqa
-from ._settings_load import (  # noqa
-    load_or_create_instance_settings,
-    load_or_create_user_settings,
-)
 from ._settings_user import UserSettings  # noqa
 from ._setup_instance import init_instance, load_instance  # noqa
 from ._setup_user import log_in_user, sign_up_user  # noqa
