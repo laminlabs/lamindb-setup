685a5523c704ab33a03fa8a064da9f7428f17e28
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-01 10:46:11 +0200

    :memo: Upgrade schema dependency

--- lndb_setup/_check_versions.py
+++ lndb_setup/_check_versions.py
@@ -1,5 +1,8 @@
-from lamindb_schema import __version__ as lamindb_schema_version
+from lamindb_schema import __version__ as lamindb_schema_v
+from lndb_schema_core import __version__ as lndb_schema_core_v
 from packaging import version
 
-if version.parse(lamindb_schema_version) < version.parse("0.3.0"):
-    raise RuntimeError("lamindb needs nbproject >= 0.3.0")
+if version.parse(lamindb_schema_v) < version.parse("0.3.0"):
+    raise RuntimeError("lamindb needs lamindb_schema_v >= 0.3.0")
+if version.parse(lndb_schema_core_v) < version.parse("0.2.1"):
+    raise RuntimeError("lamindb needs lndb_schema_core_v >= 0.2.1")
