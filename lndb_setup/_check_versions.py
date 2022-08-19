edfdacb5854cd7bc57c24e4def1c485acaf7a4a4
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-19 12:32:09 +0200

    :fire: Remove lamindb_schema

--- lndb_setup/_check_versions.py
+++ lndb_setup/_check_versions.py
@@ -1,8 +1,5 @@
-from lamindb_schema import __version__ as lamindb_schema_v
 from lnschema_core import __version__ as lnschema_core_v
 from packaging import version
 
-if version.parse(lamindb_schema_v) < version.parse("0.3.0"):
-    raise RuntimeError("lamindb needs lamindb_schema_v >= 0.3.0")
-if version.parse(lnschema_core_v) < version.parse("0.3.0"):
-    raise RuntimeError("lamindb needs lnschema_core_v >= 0.3.0")
+if version.parse(lnschema_core_v) < version.parse("0.3.2"):
+    raise RuntimeError("lamindb needs lnschema_core_v >= 0.3.2")
