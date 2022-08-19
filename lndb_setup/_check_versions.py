22287a97a2a0805c06c1510e96b089ba46a5e213
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-19 12:26:11 +0200

    :truck: Rename schema modules

--- lndb_setup/_check_versions.py
+++ lndb_setup/_check_versions.py
@@ -1,8 +1,8 @@
 from lamindb_schema import __version__ as lamindb_schema_v
-from lndb_schema_core import __version__ as lndb_schema_core_v
+from lnschema_core import __version__ as lnschema_core_v
 from packaging import version
 
 if version.parse(lamindb_schema_v) < version.parse("0.3.0"):
     raise RuntimeError("lamindb needs lamindb_schema_v >= 0.3.0")
-if version.parse(lndb_schema_core_v) < version.parse("0.3.0"):
-    raise RuntimeError("lamindb needs lndb_schema_core_v >= 0.3.0")
+if version.parse(lnschema_core_v) < version.parse("0.3.0"):
+    raise RuntimeError("lamindb needs lnschema_core_v >= 0.3.0")
