7c9f4ed3ff43def8ec440ac73207cff2cf1eedc2
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-03 18:31:29 +0200

    :bug: Ensure compat with lnbfx

--- lndb_setup/_setup_schema.py
+++ lndb_setup/_setup_schema.py
@@ -17,6 +17,13 @@ known_schema_names = [
 ]
 
 
+def get_schema_module_name(schema_name):
+    if schema_name == "bfx":
+        return "lnbfx.schema"
+    else:
+        return f"lnschema_{schema_name.replace('-', '_')}"
+
+
 def setup_schema(isettings: InstanceSettings, usettings: UserSettings):
     if isettings.schema_modules is not None:
         schema_names = isettings.schema_modules.split(", ")
@@ -26,8 +33,7 @@ def setup_schema(isettings: InstanceSettings, usettings: UserSettings):
     msg = "Loading schema modules: core"
 
     for schema_name in ["core"] + schema_names:
-        schema_module_name = f"lnschema_{schema_name.replace('-', '_')}"
-        importlib.import_module(schema_module_name)
+        importlib.import_module(get_schema_module_name(schema_name))
         msg += f", {schema_name}"
 
     logger.info(f"{msg}.")
@@ -37,8 +43,7 @@ def setup_schema(isettings: InstanceSettings, usettings: UserSettings):
     insert.user(email=usettings.email, user_id=usettings.id, handle=usettings.handle)
 
     for schema_name in ["core"] + schema_names:
-        schema_module_name = f"lnschema_{schema_name.replace('-', '_')}"
-        schema_module = importlib.import_module(schema_module_name)
+        schema_module = importlib.import_module(get_schema_module_name(schema_name))
         insert.version(
             schema_module=schema_module,
             user_id=usettings.id,  # type: ignore
