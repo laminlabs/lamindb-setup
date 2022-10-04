aedb1dbff83a8ed6dbdd0c831e55b7cdd4b048ad
Frédéric ENARD <frederic.enard@gmail.com>
2022-10-04 23:16:55 +0200

    Cast base settings dir as Path (#57)
    * :construction: Enable other settings location
    
    * :construction: Fix error in if statement
    
    * :construction: Format
    
    * :bug: Cast base settings dir as Path

--- lndb_setup/_settings_store.py
+++ lndb_setup/_settings_store.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 
 def get_settings_dir():
     if "LAMIN_BASE_SETTINGS_DIR" in os.environ:
-        return os.environ["LAMIN_BASE_SETTINGS_DIR"] / ".lndb"
+        return Path(os.environ["LAMIN_BASE_SETTINGS_DIR"]) / ".lndb"
     else:
         return Path.home() / ".lndb"
 
