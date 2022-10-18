1129a143b839bc02fcee8b2c3b33808ac5aadd98
Frédéric ENARD <frederic.enard@gmail.com>
2022-10-18 09:12:34 +0200

    :bug: Login user

--- noxfile.py
+++ noxfile.py
@@ -15,6 +15,8 @@ def lint(session: nox.Session) -> None:
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
     session.install(".[dev,test]")
+    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
+    session.run(*(login_user_1.split(" ")))
     session.run(
         "pytest",
         "-s",
