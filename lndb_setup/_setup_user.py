f0c4947e85fefc2b66bedcbd06026063a9625a8c
bpenteado <33378855+bpenteado@users.noreply.github.com>
2022-10-20 10:39:20 +0200

    fixed password error message at login

--- lndb_setup/_setup_user.py
+++ lndb_setup/_setup_user.py
@@ -76,7 +76,7 @@ def login(
 
     if user_settings.password is None:
         raise RuntimeError(
-            "No stored user password, please call: lndb login --email <your-email>"
+            "No stored user password, please call: lndb login <your-email>"
             " --password <your-password>"
         )
 
