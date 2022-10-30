0e72832caa117a5335ee663473537f398589c57a
Koncopd <koncopd@gmail.com>
2022-10-30 14:13:28 +0100

    ignore supabase error

--- lndb_setup/__init__.py
+++ lndb_setup/__init__.py
@@ -38,6 +38,9 @@ Dev API
 """
 
 __version__ = "0.13.2"
+# hide the supabase error in a thread on windows
+import os
+
 from . import _check_versions  # noqa
 from ._schema import schema  # noqa
 from ._settings import settings  # noqa
@@ -45,3 +48,16 @@ from ._settings_instance import InstanceSettings, Storage  # noqa
 from ._settings_user import UserSettings  # noqa
 from ._setup_instance import init, load  # noqa
 from ._setup_user import login, signup  # noqa
+
+if os.name == "nt":
+    import threading
+
+    original_excepthook = threading.excepthook
+
+    def except_hook(args):
+        is_overflow = args.exc_type is OverflowError
+        for_timeout = str(args.exc_value) == "timeout value is too large"
+        if not (is_overflow and for_timeout):
+            original_excepthook(args)
+
+    threading.excepthook = except_hook
