98013ab677494a9162dfb82a7bff1483638d79fb
Frédéric ENARD <frederic.enard@gmail.com>
2022-11-19 18:16:57 +0700

    :bug: Fix wrong operator

--- lndb_setup/_migrate.py
+++ lndb_setup/_migrate.py
@@ -18,7 +18,7 @@ def check_migrate(
     usettings: UserSettings,
     isettings: InstanceSettings,
 ):
-    if "LAMIN_SKIP_MIGRATION" not in os.environ:
+    if "LAMIN_SKIP_MIGRATION" in os.environ:
         if os.environ["LAMIN_SKIP_MIGRATION"] == "true":
             return "migrate-failed"
 
