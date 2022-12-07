59924d7257e3cb1ecb7fafc60db77719b88a6b36
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-07 21:24:41 +0100

    :bento: Added nox tools

---
+++ lndb_setup/_nox_tools.py
@@ -0,0 +1,24 @@
+import os
+
+from nox import Session
+
+from ._clone import setup_local_test_postgres
+
+
+def setup_test_instances_from_main_branch(session: Session):
+    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
+    session.run(*(login_user_1.split(" ")))
+    # init a test instance from the main branch
+    if "GITHUB_BASE_REF" in os.environ and os.environ["GITHUB_BASE_REF"] != "":
+        session.run("git", "checkout", os.environ["GITHUB_BASE_REF"], external=True)
+    # install the current package from main
+    session.install(".")
+    # sqlite test instance
+    session.run(*"lndb init --storage testdb".split(" "))
+    # postgres test instance
+    session.run(
+        *f"lndb init --storage pgtest --db {setup_local_test_postgres()}".split(" ")
+    )  # noqa
+    # go back to the PR branch
+    if "GITHUB_HEAD_REF" in os.environ and os.environ["GITHUB_HEAD_REF"] != "":
+        session.run("git", "checkout", os.environ["GITHUB_HEAD_REF"], external=True)
