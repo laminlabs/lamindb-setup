c33fd500d4f8b61197238d223d9ac156aeed2203
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-08 11:33:29 +0100

    :art: Move into submodule

--- lndb_setup/test/_migrations_unit.py
+++ lndb_setup/test/_migrations_unit.py
@@ -1,72 +1,21 @@
 import importlib
+import io
 from subprocess import run
-from typing import Optional
 
 import sqlalchemy as sa
+from alembic import command
 from alembic.autogenerate.api import AutogenContext
 from alembic.autogenerate.render import _render_cmd_body
-from cloudpathlib import CloudPath
-from lamin_logger import logger
-from packaging import version
 from pytest_alembic.config import Config
 from pytest_alembic.executor import CommandExecutor, ConnectionExecutor
 from pytest_alembic.plugin.error import AlembicTestFailure
 from pytest_alembic.runner import MigrationContext
 from sqlmodel import SQLModel
 
-from ._assets import instances as test_instances
-from ._clone import clone_test, setup_local_test_sqlite_file
-from ._settings_instance import InstanceSettings
-from ._setup_instance import init
+from lndb_setup._setup_instance import init
 
 
-def migrate_test(
-    schema_package: str, n_instances: Optional[int] = None, dialect_name="sqlite"
-):
-    # auto-bump version to simulate state after release
-    schema_module = importlib.import_module(schema_package)
-    v = version.parse(schema_module.__version__)
-    schema_module.__version__ = f"{v.major}.{v.minor+1}.0"  # type: ignore  # noqa
-    # get test instances
-    instances = test_instances.loc[test_instances[1] == schema_package][0]
-    if dialect_name == "sqlite":
-        run_instances = instances.loc[
-            instances.str.startswith("s3://") | instances.str.startswith("gc://")
-        ]
-    elif dialect_name == "postgresql":
-        run_instances = instances.loc[instances.str.startswith(dialect_name)]
-    else:
-        raise ValueError("Pass either 'sqlite' or 'postgresql'.")
-    display_list = [inst.split("/")[-1] for inst in run_instances.tolist()]
-    logger.info(f"These instances need to be tested: {display_list}")
-    results = []
-    for instance in run_instances.iloc[:n_instances]:
-        logger.info(f"Testing: {instance}")
-        dbconfig, storage = None, None
-        for prefix in ["s3://", "gc://"]:
-            if instance.startswith(prefix):
-                dbconfig = "sqlite"
-                storage = CloudPath(prefix + instance.replace(prefix, "").split("/")[0])
-        if dbconfig is None and instance.startswith("postgresql"):
-            dbconfig = instance
-            storage = "pgtest"
-        # init test instance
-        src_settings = InstanceSettings(storage_root=storage, _dbconfig=dbconfig)  # type: ignore  # noqa
-        connection_string = clone_test(src_settings=src_settings)
-        if dbconfig == "sqlite":
-            storage_test = setup_local_test_sqlite_file(src_settings, return_dir=True)
-            result = init(dbconfig=dbconfig, storage=storage_test, migrate=True)
-        else:
-            result = init(dbconfig=connection_string, storage=storage, migrate=True)
-        logger.info(result)
-        if dialect_name == "postgresql":
-            run("docker stop pgtest && docker rm pgtest", shell=True)
-        results.append(result)
-    return results
-
-
-def get_migration_context(schema_package, url, include_schemas=None):
-    engine = sa.create_engine(url)
+def get_migration_config(schema_package, include_schemas=None):
     raw_config = dict(
         config_file_name=f"{schema_package}/alembic.ini",
         script_location=f"{schema_package}/migrations",
@@ -75,6 +24,12 @@ def get_migration_context(schema_package, url, include_schemas=None):
     if include_schemas is not None:
         raw_config["include_schemas"] = include_schemas
     config = Config.from_raw_config(raw_config)
+    return config
+
+
+def get_migration_context(schema_package, url, include_schemas=None):
+    engine = sa.create_engine(url)
+    config = get_migration_config(schema_package, include_schemas)
     command_executor = CommandExecutor.from_config(config)
     migration_context = MigrationContext.from_config(
         config, command_executor, ConnectionExecutor(), engine
@@ -83,12 +38,23 @@ def get_migration_context(schema_package, url, include_schemas=None):
     return migration_context
 
 
+def migration_id_is_consistent(schema_package):
+    config = get_migration_config(schema_package)
+    package = importlib.import_module(schema_package)
+    output_buffer = io.StringIO()
+    # get the id of the latest migration script
+    command.heads(config.make_alembic_config(stdout=output_buffer))
+    output = output_buffer.getvalue()
+    migration_id = output.split(" ")[0]
+    return package._migration == migration_id
+
+
 def model_definitions_match_ddl(schema_package, url=None, dialect_name="sqlite"):
     if url is None and dialect_name == "sqlite":
         url = "sqlite:///testdb/testdb.lndb"
         # need to call init to reload schema
         init(dbconfig="sqlite", storage="testdb", migrate=False)
-    elif url is None and dialect_name == "postgres":
+    elif url is None and dialect_name == "postgresql":
         # requires postgres has been set up through _nox_tools
         url = "postgresql://postgres:pwd@0.0.0.0:5432/pgtest"
         # need to call init to reload schema
@@ -100,7 +66,7 @@ def model_definitions_match_ddl(schema_package, url=None, dialect_name="sqlite")
     # the below is for debugging purposes, something with indexes doesn't work 100%
     # e = sa.create_engine(url)
     # print(sa.inspect(e).get_indexes("core.dobject"))
-    include_schemas = True if dialect_name == "postgres" else False
+    include_schemas = True if dialect_name == "postgresql" else False
     migration_context = get_migration_context(
         schema_package, url, include_schemas=include_schemas
     )
--- lndb_setup/test/_nox.py
+++ lndb_setup/test/_nox.py
@@ -1,17 +1,20 @@
 import os
+from pathlib import Path
 
 from nox import Session
 
-from ._clone import setup_local_test_postgres
+from lndb_setup._clone import setup_local_test_postgres
+
+
+def login_testuser1(session: Session):
+    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
+    session.run(*(login_user_1.split(" ")), external=True)
 
 
 def setup_test_instances_from_main_branch(session: Session):
     # spin up a postgres test instance
     pgurl = setup_local_test_postgres()
-    # login a user
-    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
-    session.run(*(login_user_1.split(" ")), external=True)
-    # init a test instance from the main branch
+    # switch to the main branch
     if "GITHUB_BASE_REF" in os.environ and os.environ["GITHUB_BASE_REF"] != "":
         session.run("git", "checkout", os.environ["GITHUB_BASE_REF"], external=True)
     session.install(".")  # install current package from main branch
@@ -20,3 +23,9 @@ def setup_test_instances_from_main_branch(session: Session):
     # go back to the PR branch
     if "GITHUB_HEAD_REF" in os.environ and os.environ["GITHUB_HEAD_REF"] != "":
         session.run("git", "checkout", os.environ["GITHUB_HEAD_REF"], external=True)
+
+
+def build_docs(session: Session):
+    prefix = "." if Path("./lndocs").exists() else ".."
+    session.install(f"{prefix}/lndocs")
+    session.run("lndocs")
