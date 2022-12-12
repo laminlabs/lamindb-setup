b4c497f2292d200e803a41f7c3d1b0149bfedfb6
Frédéric ENARD <frederic.enard@gmail.com>
2022-12-12 19:09:02 +0100

    :bug: Use instance name from store

--- lndb_setup/_settings_load.py
+++ lndb_setup/_settings_load.py
@@ -72,6 +72,7 @@ def setup_instance_from_store(store: InstanceSettingsStore) -> InstanceSettings:
     settings._dbconfig = store.dbconfig_
     settings._schema = store.schema_
     settings.storage_region = store.storage_region
+    settings._name = store.name
     return settings
 
 
