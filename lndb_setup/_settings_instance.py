ba5d63370436b6a26d5e573f28e647daf34adf89
Koncopd <koncopd@gmail.com>
2022-12-13 18:05:20 +0100

    :adhesive_bandage: Fix sync warnings on instance creation

--- lndb_setup/_settings_instance.py
+++ lndb_setup/_settings_instance.py
@@ -32,6 +32,15 @@ from ._settings_store import (
 #         cursor.close()
 
 
+_MUTE_SYNC_WARNINGS = False
+
+
+def _set_mute_sync_warnings(value: bool):
+    global _MUTE_SYNC_WARNINGS
+
+    _MUTE_SYNC_WARNINGS = value
+
+
 DIRS = AppDirs("lamindb", "laminlabs")
 
 
@@ -82,12 +91,13 @@ class Storage:
                 local_filepath = Path(filepath)
         except OverwriteNewerLocalError:
             local_filepath = self.cloud_to_local_no_update(filepath)  # type: ignore
-            logger.warning(
-                f"Local file ({local_filepath}) for cloud path ({filepath}) is newer on disk than in cloud.\n"  # noqa
-                "It seems you manually updated the database locally and didn't push changes to the cloud.\n"  # noqa
-                "This can lead to data loss if somebody else modified the cloud file in"
-                " the meantime."
-            )
+            if not _MUTE_SYNC_WARNINGS:
+                logger.warning(
+                    f"Local file ({local_filepath}) for cloud path ({filepath}) is newer on disk than in cloud.\n"  # noqa
+                    "It seems you manually updated the database locally and didn't push changes to the cloud.\n"  # noqa
+                    "This can lead to data loss if somebody else modified the cloud file in"  # noqa
+                    " the meantime."
+                )
         Path(local_filepath).parent.mkdir(
             parents=True, exist_ok=True
         )  # this should not happen here but is currently needed
