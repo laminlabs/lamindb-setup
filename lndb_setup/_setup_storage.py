45636ec4ec5f29593e15f2684850fb6c6dd873d8
Frédéric ENARD <frederic.enard@gmail.com>
2022-12-13 14:55:58 +0100

    :bug: Fix wrong import

--- lndb_setup/_setup_storage.py
+++ lndb_setup/_setup_storage.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from cloudpathlib import CloudPath
+
+from ._settings_load import load_instance_settings, setup_storage_root
+from ._settings_store import current_instance_settings_file, instance_settings_file
+
+
 def get_storage_region(storage_root):
     storage_root_str = str(storage_root)
     storage_region = None
@@ -15,3 +24,16 @@ def get_storage_region(storage_root):
             storage_region = "us-east-1"
 
     return storage_region
+
+
+def set_storage(storage: Union[str, Path, CloudPath], instance_name: Optional[str]):
+    settings_file = (
+        instance_settings_file(instance_name)
+        if instance_name
+        else current_instance_settings_file()
+    )
+    isettings = load_instance_settings(settings_file)
+    storage_root = setup_storage_root(storage)
+    storage_region = get_storage_region(storage_root)
+    isettings.storage_root = storage_root
+    isettings.storage_region = storage_region
