e1db7f3a37984c1791dffdebf2be0234696940dc
Frédéric ENARD <frederic.enard@gmail.com>
2023-01-04 09:56:37 +0800

    :construction: Delete instance from the hub (WIP)

--- lndb_setup/_delete.py
+++ lndb_setup/_delete.py
@@ -2,6 +2,7 @@ import shutil
 
 from lamin_logger import logger
 
+from ._hub import connect_hub_with_auth, get_instance, get_user_by_handle
 from ._settings_load import load_instance_settings
 from ._settings_store import instance_settings_file
 
@@ -14,9 +15,14 @@ def delete(instance_name: str, owner_handle: str, delete_in_hub=True):
     shutil.rmtree(isettings.storage_root)
     logger.info("Instance root directory deleted.")
 
-    if delete_in_hub:
-        logger.info("Instance deleted from the hub.")
-        pass
+    hub = connect_hub_with_auth()
+    owner = get_user_by_handle(hub, isettings.owner)
+    instance = get_instance(hub, isettings.name, owner["id"])
+    hub.table("instance_user").delete("*").eq("instance_id", instance["id"]).execute()
+    hub.table("dtransform").delete("*").eq("instance_id", instance["id"]).execute()
+    hub.table("dobject").delete("*").eq("instance_id", instance["id"]).execute()
+    hub.table("usage").delete("*").eq("instance_id", instance["id"]).execute()
+    hub.auth.sign_out()
 
     settings_file.unlink()
     logger.info("Instance settings deleted.")
