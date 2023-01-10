42a92706f5977a4cddde0d0490f8a3d52c9507a9
Koncopd <koncopd@gmail.com>
2023-01-10 13:02:29 +0100

    correct mtime

--- lndb_setup/_upath_ext.py
+++ lndb_setup/_upath_ext.py
@@ -1,3 +1,6 @@
+from datetime import timezone
+
+from dateutil.parser import isoparse  # type: ignore
 from upath import UPath
 
 
@@ -9,5 +12,24 @@ def upload_from(self, path, **kwargs):
     self.fs.upload(path, str(self), **kwargs)
 
 
+def modified(self):
+    path = str(self)
+    try:
+        mtime = self.fs.modified(path)
+    except NotImplementedError:
+        stat = self.fs.stat(path)
+        if "updated" in stat:
+            mtime = stat["updated"]
+            mtime = isoparse(mtime)
+        else:
+            return None
+    # always convert to the local timezone before returning
+    # assume in utc if the time zone is not specified
+    if mtime.tzinfo is None:
+        mtime = mtime.replace(tzinfo=timezone.utc)
+    return mtime.astimezone().replace(tzinfo=None)
+
+
 UPath.download_to = download_to
 UPath.upload_from = upload_from
+UPath.modified = property(modified)
