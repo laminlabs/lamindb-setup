65639687a5103e2e708afb8f210f9e94b616e7aa
Frédéric ENARD <frederic.enard@gmail.com>
2023-01-11 19:37:38 +0700

    :art: Show error log for loading during delete

--- lndb_setup/_delete.py
+++ lndb_setup/_delete.py
@@ -27,9 +27,7 @@ def delete(instance_name: str):
 
 def delete_helper(hub: Client, instance_name: str):
     settings_file = instance_settings_file(instance_name, settings.user.handle)
-    message, isettings = load_isettings(
-        instance_name, settings.user.handle, log_error_message=False
-    )
+    message, isettings = load_isettings(instance_name, settings.user.handle)
     if message is not None:
         return message
 
