ae342afd57e225665f37bfa05a43411f4c89c652
Bernardo Penteado <bernardopbf3@gmail.com>
2023-01-30 19:21:37 +0000

    :sparkles: implemented rich string representation for UserSettings on CLI

--- lndb_setup/_info.py
+++ lndb_setup/_info.py
@@ -4,6 +4,5 @@ from ._settings import settings
 def info():
     """Log information about current instance."""
     # Accessing cached settings is faster than accessing the hub
-    usettings = settings.user
-    print(f"User: handle={usettings.handle} email={usettings.email} id={usettings.id}")
+    print(settings.user)
     print(f"Instance: {settings.instance.owner}/{settings.instance.name}")
