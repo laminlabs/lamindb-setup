b0f16cea134241b04007ee8eaec920d7dca25bfe
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-06 16:49:36 +0800

    :bug: Fix check_versions

--- lndb_setup/_check_versions.py
+++ lndb_setup/_check_versions.py
@@ -1,5 +1,5 @@
 from lnhub_rest import __version__ as lnhub_rest_v
 from packaging import version
 
-if version.parse(lnhub_rest_v) != version.parse("0.1.4"):
-    raise RuntimeError("Upgrade lnhub_rest! pip install lnhub_rest==0.1.4")
+if version.parse(lnhub_rest_v) != version.parse("0.2.0"):
+    raise RuntimeError("Upgrade lnhub_rest: pip install lnhub_rest==0.2.0")
