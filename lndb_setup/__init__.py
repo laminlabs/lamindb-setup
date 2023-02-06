ef2bd88629d502288de5025e19badc9c81fc0459
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-06 16:41:19 +0800

    :bookmark: Release version 0.30.13

--- lndb_setup/__init__.py
+++ lndb_setup/__init__.py
@@ -59,7 +59,7 @@ Dev API
    Storage
 """
 
-__version__ = "0.30.12"  # denote a pre-release for 0.1.0 with 0.1a1
+__version__ = "0.30.13"  # denote a pre-release for 0.1.0 with 0.1a1
 
 import sys
 from os import name as _os_name
