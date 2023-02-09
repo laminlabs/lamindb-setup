73a98e4804099a89924eef4bbbea2213e416711d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-09 21:50:21 +0800

    :art: better logging when script is empty

--- lndb_setup/_migrations.py
+++ lndb_setup/_migrations.py
@@ -15,20 +15,11 @@ def _get_package_info(schema_root: Optional[Path] = None):
         package_name = f"{package_name}.schema"
         package = importlib.import_module(package_name)
     schema_id = getattr(package, "_schema_id")
-    package_path = Path(package.__file__).parent.parent  # type:ignore
-    return package_name, package_path, schema_id
+    migrations_path = Path(package.__file__).parent / "migrations"  # type:ignore
+    return package_name, migrations_path, schema_id
 
 
-def _generate_module_files(package_name: str, package_path: Path, schema_id: str):
-    def read(filename: Path):
-        with open(filename, "r") as f:
-            return f.read()
-
-    def write(filename: Path, content: str):
-        with open(filename, "w") as f:
-            return f.write(content)
-
-    migrations_path = package_path / package_name / "migrations"
+def _generate_module_files(package_name: str, migrations_path: Path, schema_id: str):
     _migrations_path = Path(__file__).parent / "_migrations"
 
     # ensures migrations/versions folder exists
@@ -36,7 +27,7 @@ def _generate_module_files(package_name: str, package_path: Path, schema_id: str
 
     if not (migrations_path / "env.py").exists():
         content = (
-            read(_migrations_path / "env.py")
+            _readfile(_migrations_path / "env.py")
             .replace("_schema_id = None\n", "")
             .replace("# from {package_name} import *", f"from {package_name} import *")
             .replace(
@@ -44,7 +35,7 @@ def _generate_module_files(package_name: str, package_path: Path, schema_id: str
                 f"from {package_name} import _schema_id",
             )
         )
-        write(migrations_path / "env.py", content)
+        _writefile(migrations_path / "env.py", content)
 
     if not (migrations_path / "script.py.mako").exists():
         import shutil
@@ -55,11 +46,41 @@ def _generate_module_files(package_name: str, package_path: Path, schema_id: str
 
     if not (migrations_path.parent / "alembic.ini").exists():
         content = (
-            read(_migrations_path / "alembic.ini")
+            _readfile(_migrations_path / "alembic.ini")
             .replace("[{schema_id}]", f"[{schema_id}]")
             .replace("{package_name}/migrations", f"{package_name}/migrations")
         )
-        write(migrations_path.parent / "alembic.ini", content)
+        _writefile(migrations_path.parent / "alembic.ini", content)
+
+
+def _set_alembic_logging_level(migrations_path: Path, level="INFO"):
+    alembic_ini_path = migrations_path.parent / "alembic.ini"
+    text = _readfile(alembic_ini_path)
+    current_level = text.split("[logger_alembic]\n")[1].split("\n")[0]
+    new_level = f"level = {level}"
+    _writefile(
+        alembic_ini_path,
+        text.replace(
+            f"[logger_alembic]\n{current_level}", f"[logger_alembic]\n{new_level}"
+        ),
+    )
+
+
+def _readfile(filename: Path):
+    with open(filename, "r") as f:
+        return f.read()
+
+
+def _writefile(filename: Path, content: str):
+    with open(filename, "w") as f:
+        return f.write(content)
+
+
+push_instruction = """Please push your changes to a new remote branch and open a PR.
+Inspect CI step Build output and add migration code to the script.
+It will look like the following, beware of the renaming:
+    op.drop_column('biosample', 'description', schema='wetlab')
+    op.add_column('experiment', sa.Column('donor_id', sqlmodel.sql.sqltypes.AutoString(), schema="wetlab")"""  # noqa
 
 
 class migrate:
@@ -76,18 +97,22 @@ class migrate:
             version: Version string to label migration with.
             schema_root: Optional. Root directory of schema module.
         """
-        package_name, package_path, schema_id = _get_package_info(
+        package_name, migrations_path, schema_id = _get_package_info(
             schema_root=schema_root
         )
         _generate_module_files(
-            package_name=package_name, package_path=package_path, schema_id=schema_id
+            package_name=package_name,
+            migrations_path=migrations_path,
+            schema_id=schema_id,
         )
-        db_path = package_path / "testdb/testdb.lndb"  # type: ignore # noqa
+        db_path = migrations_path.parent.parent / "testdb/testdb.lndb"  # type: ignore # noqa
         if db_path.exists():
             rm = False
+            _set_alembic_logging_level(migrations_path, level="INFO")
             logger.info("Generate migration with reference db: testdb/testdb.lndb")
         else:
             rm = True
+            _set_alembic_logging_level(migrations_path, level="WARN")
             logger.info("Generate empty migration script.")
         command = (
             f"alembic --config {package_name}/alembic.ini --name {schema_id} revision"
@@ -103,10 +128,7 @@ class migrate:
             logger.success(f"Successfully generated migration {version}.")
             if rm:
                 run(f"rm {db_path.as_posix()}", shell=True, cwd=cwd)
-                logger.info(
-                    "Please commit and push your changes and add migration code from CI"
-                    " to the script."
-                )
+                logger.info(push_instruction)
             return None
         else:
             logger.error("Generating migration failed.")
