8f6f9623a544c2cbbd1aaed4ee1243ac7acfd285
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-14 08:41:27 +0100

    🚑 Generate all migrate files before migrate (#274)
    :ambulance: Generate all migrate files before migrate

--- lndb/_migrate.py
+++ lndb/_migrate.py
@@ -12,7 +12,7 @@ from natsort import natsorted
 from packaging.version import parse as vparse
 
 from ._db import insert
-from ._migrations import generate_alembic_ini, modify_alembic_ini
+from ._migrations import generate_module_files, modify_alembic_ini
 from ._settings_instance import InstanceSettings
 from ._settings_user import UserSettings
 from ._setup_schema import create_schema_if_not_exists, get_schema_module_name
@@ -98,7 +98,7 @@ def check_migrate(
                 )
                 return "migrate-failed"
 
-            generate_alembic_ini(
+            generate_module_files(
                 package_name=schema_module_name,
                 migrations_path=Path(schema_module.__file__).parent / "migrations",  # type: ignore  # noqa
                 schema_id=schema_id,
