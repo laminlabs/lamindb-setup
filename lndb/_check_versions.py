1657abf4c40ffecd00d432d24b03f81a339cb430
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-16 16:51:32 +0100

    🚸 Proper client server check (#284)
    * :children_crossing: Proper client server check
    
    * :green_heart: Remove
    
    * :arrow_up: Upgrade lnhub_rest
    
    * :green_heart: Fix

--- lndb/_check_versions.py
+++ lndb/_check_versions.py
@@ -1,5 +1,5 @@
 from lnhub_rest import __version__ as lnhub_rest_v
 from packaging import version
 
-if version.parse(lnhub_rest_v) != version.parse("0.4.0"):
-    raise RuntimeError("Upgrade lnhub_rest: pip install lnhub_rest==0.4.0")
+if version.parse(lnhub_rest_v) < version.parse("0.4.3"):
+    raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest -U")
