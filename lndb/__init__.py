67d020c95e58aa08ab4ad0307bf270af059d7604
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-16 23:31:45 +0100

    :bookmark: Release version 0.34.0

--- lndb/__init__.py
+++ lndb/__init__.py
@@ -55,7 +55,7 @@ Developer API.
    dev
 """
 
-__version__ = "0.33.0"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.34.0"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 import sys
 from os import name as _os_name
