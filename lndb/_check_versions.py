aa2a5de33c1c1085316917cdc7f6f70094d53558
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-16 16:03:36 +0100

    :children_crossing: Proper client server check

--- lndb/_check_versions.py
+++ lndb/_check_versions.py
@@ -1,5 +1,13 @@
 from lnhub_rest import __version__ as lnhub_rest_v
+from lnhub_rest import check_breaks_lndb
 from packaging import version
 
-if version.parse(lnhub_rest_v) != version.parse("0.4.0"):
-    raise RuntimeError("Upgrade lnhub_rest: pip install lnhub_rest==0.4.0")
+if check_breaks_lndb():
+    raise SystemExit(
+        "Your lamindb installation is out-of-date.\n"
+        "Please upgrade: pip install lamindb -U"
+    )
+
+
+if version.parse(lnhub_rest_v) >= version.parse("0.4.2"):
+    raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest -U")
