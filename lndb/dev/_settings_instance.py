952d3e3a5c8960807c85600e66b47bc42fb875fc
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-16 22:06:06 +0100

    :green_heart: Fix

--- lndb/dev/_settings_instance.py
+++ lndb/dev/_settings_instance.py
@@ -221,7 +221,7 @@ class InstanceSettings:
         shutil.copy2(filepath, current_instance_settings_file())
         # persist under settings class for same session reference
         # need to import here to avoid circular import
-        from ._settings import settings
+        from .._settings import settings
 
         settings._instance_settings = self
 
