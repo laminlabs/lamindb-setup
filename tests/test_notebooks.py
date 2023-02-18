d7657f28e16f20a6dd50a609486758af8773cb04
Koncopd <koncopd@gmail.com>
2023-02-18 13:28:07 +0100

    guide faq should be in this order

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,11 +6,9 @@ from nbproject.dev import test
 
 def test_notebooks():
     # assuming this is in the tests folder
-    docs_folder = Path(__file__).parents[1] / "docs/"
+    docsdir = Path(__file__).parents[1] / "docs/"
 
-    for check_folder in docs_folder.glob("./**"):
-        # these are the notebook testpaths
-        if not str(check_folder).endswith(("guide", "faq")):
-            continue
-        logger.debug(f"\n{check_folder}")
-        test.execute_notebooks(check_folder, write=True)
+    for subdir in ["guide", "faq"]:
+        checkdir = docsdir / subdir
+        logger.info(f"\n---{checkdir.stem}---")
+        test.execute_notebooks(checkdir, write=True)
