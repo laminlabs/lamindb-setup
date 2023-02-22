14273a2b975cb42b7660cfd6068b7c278f42f82e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-22 23:11:49 +0100

    :construction_worker: Add CI helper for uploading docs

--- lndb/test/_nox.py
+++ lndb/test/_nox.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 from typing import Optional
 
@@ -69,3 +70,27 @@ def build_docs(session: Session):
     prefix = "." if Path("./lndocs").exists() else ".."
     session.install(f"{prefix}/lndocs")
     session.run("lndocs")
+
+
+def upload_docs_dir():
+    import lndb
+
+    if os.environ["GITHUB_EVENT_NAME"] != "push":
+        return
+    package_name = get_package_name()
+    filestem = f"{package_name}_docs"
+    filename = shutil.make_archive(filestem, "zip", "./docs")
+    lndb.load("testuser1/lamin-site-assets", migrate=True)
+
+    import lamindb as ln
+    import lamindb.schema as lns
+
+    with ln.Session() as ss:
+        dobject = ss.select(ln.DObject, name=filestem).one_or_none()
+        pipeline = ln.add(lns.Pipeline, name=f"CI {package_name}")
+        run = lns.Run(pipeline=pipeline)
+        if dobject is None:
+            dobject = ln.DObject(filename, source=run)
+        else:
+            dobject.run = run
+        ss.add(dobject)
