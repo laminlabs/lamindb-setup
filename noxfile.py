de542fafca774c51e875d235ac90b2075ec7881c
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-27 21:52:01 +0100

    :construction_worker: CI setup

--- noxfile.py
+++ noxfile.py
@@ -1,34 +1,9 @@
-import os
-import shutil
 from pathlib import Path
 
 import nox
+from laminci import upload_docs_dir
 
 nox.options.reuse_existing_virtualenvs = True
-
-
-def upload_docs_dir():
-    import lndb
-
-    if os.environ["GITHUB_EVENT_NAME"] != "push":
-        return
-    package_name = "lndb"
-    filestem = f"{package_name}_docs"
-    filename = shutil.make_archive(filestem, "zip", "./docs")
-    lndb.load("testuser1/lamin-site-assets", migrate=True)
-
-    import lamindb as ln
-    import lamindb.schema as lns
-
-    with ln.Session() as ss:
-        dobject = ss.select(ln.DObject, name=filestem).one_or_none()
-        pipeline = ln.add(lns.Pipeline, name=f"CI {package_name}")
-        run = lns.Run(pipeline=pipeline)
-        if dobject is None:
-            dobject = ln.DObject(filename, source=run)
-        else:
-            dobject.source = run
-        ss.add(dobject)
 
 
 @nox.session
