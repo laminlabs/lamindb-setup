5b03c8bf46b8c1ec92ec4966a2ed719cd956af65
Frédéric ENARD <frederic.enard@gmail.com>
2023-03-07 15:09:02 +0100

    :construction_worker: Enable testing using a specific environment

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import nox
@@ -7,6 +8,17 @@ if __version__ < "0.2.4":
     raise RuntimeError("Upgrade laminci version.")
 
 nox.options.reuse_existing_virtualenvs = True
+
+lamin_env = "prod"
+if "GITHUB_REF_NAME" in os.environ:
+    if os.environ["GITHUB_REF_NAME"] == "main":
+        lamin_env = "prod"
+    elif os.environ["GITHUB_REF_NAME"] == "staging":
+        lamin_env = "staging"
+elif "LAMIN_ENV" in os.environ:
+    lamin_env = os.environ["LAMIN_ENV"]
+
+env = {"LAMIN_ENV": lamin_env}
 
 
 @nox.session
@@ -20,9 +32,9 @@ def lint(session: nox.Session) -> None:
 def build(session):
     session.install(".[dev,test]")
     login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
-    session.run(*(login_user_1.split(" ")))
+    session.run(*(login_user_1.split(" ")), env=env)
     login_user_2 = "lndb login testuser2@lamin.ai --password goeoNJKE61ygbz1vhaCVynGERaRrlviPBVQsjkhz"  # noqa
-    session.run(*(login_user_2.split(" ")), external=True)
+    session.run(*(login_user_2.split(" ")), external=True, env=env)
     session.run(
         "pytest",
         "-s",
@@ -33,5 +45,5 @@ def build(session):
     session.run("coverage", "xml")
     prefix = "." if Path("./lndocs").exists() else ".."
     session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
+    session.run("lndocs", env=env)
     upload_docs_dir()
