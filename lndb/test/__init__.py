14a8d9aa183037c874af59774580f402d9ddbde9
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-26 17:55:59 +0200

    :art: Proper migrate_clone

--- lndb/test/__init__.py
+++ lndb/test/__init__.py
@@ -1,8 +1,19 @@
-"""Testing tools for Lamin's Python packages."""
-from lndb.test._migrations_e2e import migrate_clones
+"""Test Lamin instances.
+
+.. autosummary::
+   :toctree:
+
+   migrate_clone
+   migrate_clones
+   migration_id_is_consistent
+   model_definitions_match_ddl
+
+"""
+from lndb.test._migrations_e2e import migrate_clone, migrate_clones
 from lndb.test._migrations_unit import (
     migration_id_is_consistent,
     model_definitions_match_ddl,
 )
 
+# backward compat
 from ._env import get_package_name
