afe69f0965f6d494b8a4346b51c39d33587b42f7
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-04 17:59:49 +0200

    :green_heart: Try to fix

--- lndb/_migrate/__init__.py
+++ lndb/_migrate/__init__.py
@@ -1,3 +1,3 @@
-from lndb._migrate.core import migrate
-from lndb._migrate.deploy import check_deploy_migration
-from lndb._migrate.utils import generate_module_files, get_schema_package_info
+from .core import migrate
+from .deploy import check_deploy_migration
+from .utils import generate_module_files, get_schema_package_info
--- lndb/test/__init__.py
+++ lndb/test/__init__.py
@@ -9,11 +9,7 @@
    model_definitions_match_ddl
 
 """
-from lndb.test._migrations_e2e import migrate_clone, migrate_clones
-from lndb.test._migrations_unit import (
-    migration_id_is_consistent,
-    model_definitions_match_ddl,
-)
-
 # backward compat
 from ._env import get_package_name
+from ._migrations_e2e import migrate_clone, migrate_clones
+from ._migrations_unit import migration_id_is_consistent, model_definitions_match_ddl
