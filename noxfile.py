b95a14d5c8a57e62fe8f205d0c7d86eba1375a9d
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-10 15:46:39 +0200

    :construction_worker: Update CI config

--- noxfile.py
+++ noxfile.py
@@ -1,50 +1,41 @@
 import os
-from pathlib import Path
 
 import nox
-from laminci import __version__, upload_docs_dir
-
-if __version__ < "0.2.4":
-    raise RuntimeError("Upgrade laminci version.")
+from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
+from laminci.nox import (
+    build_docs,
+    login_testuser1,
+    login_testuser2,
+    run_pre_commit,
+    run_pytest,
+)
 
 nox.options.reuse_existing_virtualenvs = True
 
-lamin_env = "prod"
+
+LAMIN_ENV = "prod"
 if "GITHUB_REF_NAME" in os.environ:
     if os.environ["GITHUB_REF_NAME"] == "main":
-        lamin_env = "prod"
+        LAMIN_ENV = "prod"
     elif os.environ["GITHUB_REF_NAME"] == "staging":
-        lamin_env = "staging"
+        LAMIN_ENV = "staging"
 elif "LAMIN_ENV" in os.environ:
-    lamin_env = os.environ["LAMIN_ENV"]
+    LAMIN_ENV = os.environ["LAMIN_ENV"]
 
-env = {"LAMIN_ENV": lamin_env}
+env = {"LAMIN_ENV": LAMIN_ENV}
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    run_pre_commit(session)
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
     session.install(".[dev,test]")
-    login_user_1 = "lndb login testuser1@lamin.ai --password cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"  # noqa
-    session.run(*(login_user_1.split(" ")), env=env)
-    login_user_2 = "lndb login testuser2@lamin.ai --password goeoNJKE61ygbz1vhaCVynGERaRrlviPBVQsjkhz"  # noqa
-    session.run(*(login_user_2.split(" ")), external=True, env=env)
-    session.run(
-        "pytest",
-        "-s",
-        "--cov=lndb",
-        "--cov-append",
-        "--cov-report=term-missing",
-        env=env,
-    )
-    session.run("coverage", "xml")
-    prefix = "." if Path("./lndocs").exists() else ".."
-    session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
-    upload_docs_dir()
+    login_testuser1(session, env=env)
+    login_testuser2(session, env=env)
+    run_pytest(session, env=env)
+    build_docs(session)
+    upload_docs_artifact()
+    move_built_docs_to_docs_slash_project_slug()
