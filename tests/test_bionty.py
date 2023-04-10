f6896d837d1253183b848ddc525a1bd153575c3c
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-10 15:57:49 +0200

    :green_heart: Fix tests

--- tests/test_bionty.py
+++ tests/test_bionty.py
@@ -1,14 +1,17 @@
-import lnschema_bionty
 import sqlmodel as sqm
 
-from lndb import init, login, settings
+import lndb
 
 
 def test_init_bionty():
-    login("testuser1@lamin.ai", password="cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS")
-    init(storage="test-bionty", schema="bionty")
+    lndb.login(
+        "testuser1@lamin.ai", password="cEvcwMJFX4OwbsYVaMt2Os6GxxGgDUlBGILs2RyS"
+    )
+    lndb.init(storage="test-bionty", schema="bionty")
 
-    with sqm.Session(settings.instance.engine) as session:
+    import lnschema_bionty
+
+    with sqm.Session(lndb.settings.instance.engine) as session:
         session.exec(
             sqm.select(lnschema_bionty.dev.CurrentBiontyVersions).where(
                 lnschema_bionty.dev.CurrentBiontyVersions.entity == "Gene"
