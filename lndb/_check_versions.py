99b69a203774f0b716ef0d6bbc08cc147ba9d541
fredericenard <frederic.enard@gmail.com>
2023-04-25 19:15:47 +0700

    :bug: Update check version

--- lndb/_check_versions.py
+++ lndb/_check_versions.py
@@ -1,5 +1,5 @@
 from lnhub_rest import __version__ as lnhub_rest_v
 from packaging import version
 
-if version.parse(lnhub_rest_v) != version.parse("0.8.2"):
+if version.parse(lnhub_rest_v) != version.parse("0.9.2"):
     raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest==0.8.2")
