df3ebfdae0505feb6976d2628b5c584178e98a2f
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-28 14:51:52 +0200

    ⬆️ Upgrade lnhub-rest to 0.9.7 (#383)
    :arrow_up: Upgrade lnhub-rest

--- lndb/_check_versions.py
+++ lndb/_check_versions.py
@@ -1,5 +1,5 @@
 from lnhub_rest import __version__ as lnhub_rest_v
 from packaging import version
 
-if version.parse(lnhub_rest_v) != version.parse("0.9.4"):
-    raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest==0.9.4")
+if version.parse(lnhub_rest_v) != version.parse("0.9.7"):
+    raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest==0.9.7")
