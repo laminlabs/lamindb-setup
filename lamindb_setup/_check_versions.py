c4f19970a1b07eaf1e2981cc7531e9279dbf94e7
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-04 20:00:42 +0200

    :zap: Upgrade to lnhub-rest 0.9.9 and remove version check

--- lamindb_setup/_check_versions.py
+++
@@ -1,5 +0,0 @@
-from lnhub_rest import __version__ as lnhub_rest_v
-from packaging import version
-
-if version.parse(lnhub_rest_v) != version.parse("0.9.8"):
-    raise SystemExit("Please upgrade lnhub_rest: pip install lnhub_rest==0.9.8")
