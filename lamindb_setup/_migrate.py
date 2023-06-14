9b8750fd182687ff49f306b0c4c72c7f4cba3f88
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 22:49:10 +0200

    ✨ Add ability to check for migrations (#419)
    * :sparkles: Add ability to check for migrations
    
    * :green_heart: Add logging
    
    * :white_check_mark: Add a test
    
    * :memo: Add docstring
    
    * :white_check_mark: Try to expand tests
    
    * :green_heart: Fix

--- lamindb_setup/_migrate.py
+++ lamindb_setup/_migrate.py
@@ -1,14 +1,43 @@
+from lamin_logger import logger
+
 from ._settings import settings
 from .dev._django import setup_django
 
 
 class migrate:
-    """Manage migrations."""
+    """Manage migrations.
+
+    Examples:
+
+    >>> import lamindb as ln
+    >>> ln.setup.migrate.create()
+    >>> ln.setup.migrate.deploy()
+    >>> ln.setup.migrate.check()
+
+    """
 
     @classmethod
-    def create(cls):
+    def create(cls) -> None:
+        """Create a migration."""
         setup_django(settings.instance, create_migrations=True)
 
     @classmethod
-    def deploy(cls):
+    def deploy(cls) -> None:
+        """Deploy a migration."""
         setup_django(settings.instance, deploy_migrations=True)
+
+    @classmethod
+    def check(cls) -> bool:
+        """Check whether ORM definitions are in sync with migrations."""
+        from django.core.management import call_command
+
+        setup_django(settings.instance)
+        try:
+            call_command("makemigrations", check_changes=True)
+        except SystemExit:
+            logger.error(
+                "Migrations are not in sync with ORMs, please create a migration: lamin"
+                " migrate create"
+            )
+            return False
+        return True
