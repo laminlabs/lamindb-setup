92bd550db5df95034c4cd7fb8e90007111f9a0a7
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 16:23:39 +0200

    :sparkles: Add ability to check for migrations

--- lamindb_setup/_migrate.py
+++ lamindb_setup/_migrate.py
@@ -6,9 +6,23 @@ class migrate:
     """Manage migrations."""
 
     @classmethod
-    def create(cls):
+    def create(cls) -> None:
+        """Create a migration."""
         setup_django(settings.instance, create_migrations=True)
 
     @classmethod
-    def deploy(cls):
+    def deploy(cls) -> None:
+        """Deploy a migration."""
         setup_django(settings.instance, deploy_migrations=True)
+
+    @classmethod
+    def check(cls) -> bool:
+        """Check whether ORM definitions are in sync with migrations."""
+        from django.core.management import call_command
+
+        setup_django(settings.instance)
+        try:
+            call_command("makemigrations", check_changes=True)
+        except SystemExit:
+            return False
+        return True
