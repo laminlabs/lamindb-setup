14c8cb4efe193faf1cbf248881a649ac9959b304
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 16:37:33 +0200

    :green_heart: Add logging

--- lamindb_setup/_migrate.py
+++ lamindb_setup/_migrate.py
@@ -1,3 +1,5 @@
+from lamin_logger import logger
+
 from ._settings import settings
 from .dev._django import setup_django
 
@@ -24,5 +26,9 @@ class migrate:
         try:
             call_command("makemigrations", check_changes=True)
         except SystemExit:
+            logger.error(
+                "Migrations are not in sync with ORMs, please create a migration: lamin"
+                " migrate create"
+            )
             return False
         return True
