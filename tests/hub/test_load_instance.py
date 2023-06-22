1f6446d005fdc1e6f10ff873bac5a131849a99eb
Bernardo Penteado <bernardopbf3@gmail.com>
2023-06-22 18:13:10 -0300

    :white_check_mark: fixed load_instance unit tests

--- tests/hub/test_load_instance.py
+++ tests/hub/test_load_instance.py
@@ -1,12 +1,18 @@
 from lamindb_setup.dev._hub_core import load_instance
+from lamindb_setup.dev._hub_crud import sb_select_instance_by_name
 
 
-def test_load_instance(instance_1, auth_1):
+def test_load_instance(user_account_1, instance_name_1, account_hub_1, auth_1):
+    instance = sb_select_instance_by_name(
+        account_id=user_account_1["id"],
+        name=instance_name_1,
+        supabase_client=account_hub_1,
+    )
     result = load_instance(
         owner=auth_1["handle"],
-        name=instance_1["name"],
+        name=instance["name"],
         _access_token=auth_1["access_token"],
     )
-    instance, _ = result
-    assert instance.name == instance_1["name"]
-    assert instance.db
+    loaded_instance, _ = result
+    assert loaded_instance.name == instance["name"]
+    assert loaded_instance.db
