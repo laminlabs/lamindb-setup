ec8b49ca8e08fb10916813128abe57d830aff57e
Bernardo Penteado <bernardopbf3@gmail.com>
2023-06-23 10:06:24 -0300

    :white_check_mark: consolidate instance tests

--- tests/hub/test_instance.py
+++ tests/hub/test_instance.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lamindb_setup.dev._hub_core import init_instance
+from lamindb_setup.dev._hub_core import init_instance, load_instance
 from lamindb_setup.dev._hub_crud import sb_select_instance_by_name
 
 
@@ -23,3 +23,14 @@ def instance_1(auth_1, instance_name_1, user_account_1, account_hub_1):
         supabase_client=account_hub_1,
     )
     return instance
+
+
+def test_load_instance(auth_1, instance_1):
+    result = load_instance(
+        owner=auth_1["handle"],
+        name=instance_1["name"],
+        _access_token=auth_1["access_token"],
+    )
+    loaded_instance, _ = result
+    assert loaded_instance.name == instance_1["name"]
+    assert loaded_instance.db
