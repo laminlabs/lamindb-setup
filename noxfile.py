8ff1b80afbbfebf5ab2fdcf07da3110c88473ba2
Bernardo Penteado <bernardopbf3@gmail.com>
2023-07-17 16:23:10 +0100

    :construction_worker: updated noxfile to run tests against staging

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,8 @@
-import os
-
 import nox
 from laminci import upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, login_testuser2, run_pre_commit
 
 nox.options.default_venv_backend = "none"
-
-
-LAMIN_ENV = "prod"
-if "GITHUB_REF_NAME" in os.environ:
-    if os.environ["GITHUB_REF_NAME"] == "main":
-        LAMIN_ENV = "prod"
-    elif os.environ["GITHUB_REF_NAME"] == "staging":
-        LAMIN_ENV = "staging"
-elif "LAMIN_ENV" in os.environ:
-    LAMIN_ENV = os.environ["LAMIN_ENV"]
-
-env = {"LAMIN_ENV": LAMIN_ENV}
 
 
 @nox.session
@@ -46,16 +32,23 @@ def install(session: nox.Session, group: str) -> None:
     "group",
     ["unit", "docs"],
 )
-def build(session: nox.Session, group: str):
-    login_testuser1(session, env=env)
-    login_testuser2(session, env=env)
+@nox.parametrize(
+    "lamin_env",
+    ["staging", "prod"],
+)
+def build(session: nox.Session, group: str, lamin_env: str):
+    login_testuser1(session, env={"LAMIN_ENV": lamin_env})
+    login_testuser2(session, env={"LAMIN_ENV": lamin_env})
     coverage_args = "--cov=lamindb_setup --cov-append --cov-report=term-missing"  # noqa
     if group.startswith("unit"):
         session.run(
-            *f"pytest -s {coverage_args} ./tests --ignore tests/hub".split(), env=env
+            *f"pytest -s {coverage_args} ./tests --ignore tests/hub".split(),
+            env={"LAMIN_ENV": lamin_env},
         )
     elif group.startswith("docs"):
-        session.run(*f"pytest -s {coverage_args} ./docs".split(), env=env)
+        session.run(
+            *f"pytest -s {coverage_args} ./docs".split(), env={"LAMIN_ENV": lamin_env}
+        )
 
 
 @nox.session
