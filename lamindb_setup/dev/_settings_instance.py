5b12f364638164e2f855774d0d4edbb1f5f7c1b9
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-16 11:08:54 +0200

    💚 Fix typo

--- lamindb_setup/dev/_settings_instance.py
+++ lamindb_setup/dev/_settings_instance.py
@@ -237,7 +237,7 @@ class InstanceSettings:
             from django.db import connection
 
             try:
-                connection.connect()
+                connection.cursor()
             except Exception:
                 if not mute:
                     logger.warning(f"connection {self.db} not reachable")
