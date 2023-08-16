7da7b39fdcc15732c9be17a8d4fdb8056e8098c5
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-16 11:31:11 +0200

    🔥 Remove legacy instance support

--- lamindb_setup/dev/_django.py
+++ lamindb_setup/dev/_django.py
@@ -34,129 +34,6 @@ def get_migrations_to_sync():
     return missing_migrations
 
 
-def check_is_legacy_instance_and_fix(isettings) -> bool:
-    import sqlalchemy as sa
-
-    engine = sa.create_engine(isettings.db)
-
-    # this checks whether its a legacy instance before lamindb 0.41.0
-    try:
-        with engine.connect() as conn:
-            conn.execute(sa.text("select * from core.user")).first()
-        raise RuntimeError(
-            "Please first load your instance with lamindb 0.41.2, after that, you can"
-            " upgrade to lamindb >=0.42"
-        )
-    except Exception:
-        pass
-    # this checks whether django_migrations is already available
-    try:
-        with engine.connect() as conn:
-            conn.execute(sa.text("select * from lnschema_core_user")).first()
-        user_table_exists = True
-    except Exception:
-        user_table_exists = False
-    try:
-        with engine.connect() as conn:
-            conn.execute(sa.text("select * from django_migrations")).first()
-        django_table_exists = True
-    except Exception:
-        django_table_exists = False
-
-    # neither user table nor django table there, it's an empty new instance
-    if not user_table_exists and not django_table_exists:
-        return False
-    # the django table is there, it's a django instance
-    if django_table_exists:
-        return False
-    # otherwise, it's a legacy instance: it has the user table but not the django table
-
-    datetime_str = "datetime" if isettings.dialect == "sqlite" else "timestamp"
-
-    # now let's proceed
-    # fmt: off
-    stmts = [
-        f"alter table lnschema_core_run add column run_at {datetime_str}",
-        "drop index if exists ix_core_run_transform_v",
-        "drop index if exists ix_lnschema_core_run_transform_version",
-        "drop index if exists ix_lnschema_core_file_transform_version",
-        "alter table lnschema_core_run drop column transform_version",
-        "alter table lnschema_core_file drop column transform_version",
-        "update lnschema_core_file set created_by_id = 'DzTjkKse' where created_by_id is null",
-        "alter table lnschema_core_project add column external_id varchar(40)",
-        "alter table lnschema_core_transform rename column name to short_name",
-        "alter table lnschema_core_transform rename column title to name",
-        "alter table lnschema_core_runinput add column id bigint",
-        "update lnschema_core_transform set name = short_name where name is null",
-        "alter table lnschema_core_transform add column stem_id varchar(12)",
-        "update lnschema_core_transform set stem_id = id",
-        "alter table lnschema_core_features rename to lnschema_core_featureset",
-        f"alter table lnschema_core_featureset add column updated_at {datetime_str}",
-        # now rename
-        "alter table lnschema_core_user rename to lnschema_core_legacy_user",
-        "alter table lnschema_core_storage rename to lnschema_core_legacy_storage",
-        "alter table lnschema_core_project rename to lnschema_core_legacy_project",
-        "alter table lnschema_core_transform rename to lnschema_core_legacy_transform",
-        "alter table lnschema_core_run rename to lnschema_core_legacy_run",
-        "alter table lnschema_core_featureset rename to lnschema_core_legacy_featureset",
-        "alter table lnschema_core_folder rename to lnschema_core_legacy_folder",
-        "alter table lnschema_core_file rename to lnschema_core_legacy_file",
-        "alter table lnschema_core_runinput rename to lnschema_core_legacy_runinput",
-    ]
-    if "bionty" in isettings.schema:
-        stmts += [
-            "alter table lnschema_bionty_species rename to lnschema_bionty_legacy_species",  # noqa
-            "alter table lnschema_bionty_gene rename to lnschema_bionty_legacy_gene",  # noqa
-            "alter table lnschema_bionty_protein rename to lnschema_bionty_legacy_protein",  # noqa
-            "alter table lnschema_bionty_cellmarker rename to lnschema_bionty_legacy_cellmarker",  # noqa
-            "alter table lnschema_bionty_tissue rename to lnschema_bionty_legacy_tissue",  # noqa
-            "alter table lnschema_bionty_celltype rename to lnschema_bionty_legacy_celltype",  # noqa
-            "alter table lnschema_bionty_disease rename to lnschema_bionty_legacy_disease",  # noqa
-            "alter table lnschema_bionty_cellline rename to lnschema_bionty_legacy_cellline",  # noqa
-            "alter table lnschema_bionty_phenotype rename to lnschema_bionty_legacy_phenotype",  # noqa
-            "alter table lnschema_bionty_pathway rename to lnschema_bionty_legacy_pathway",  # noqa
-            "alter table lnschema_bionty_readout rename to lnschema_bionty_legacy_readout",  # noqa
-        ]
-    # fmt: on
-    with engine.connect() as conn:
-        for stmt in stmts:
-            try:
-                conn.execute(sa.text(stmt))
-            except Exception as e:
-                logger.warning(f"failed to execute: {stmt} because of {e}")
-    logger.success("created legacy migration preparations")
-    return True
-
-
-def insert_legacy_data(isettings: InstanceSettings):
-    import sqlalchemy as sa
-
-    engine = sa.create_engine(isettings.db)
-    # fmt: off
-    stmts = [
-        # we use the handle instead of the name below to deal with SQLite's inability of changing nullability
-        "insert into lnschema_core_user (id, handle, email, name, created_at, updated_at) select id, handle, email, handle, created_at, created_at from lnschema_core_legacy_user",
-        "insert into lnschema_core_storage (id, root, type, region, created_at, updated_at, created_by_id) select id, root, type, region, created_at, created_at, created_by_id from lnschema_core_legacy_storage",
-        "update lnschema_core_legacy_project set updated_at = created_at",
-        "insert into lnschema_core_project select * from lnschema_core_legacy_project",
-        "insert into lnschema_core_transform (id, name, short_name, stem_id, version, type, reference, created_at, updated_at, created_by_id) select id, name, short_name, stem_id, version, type, reference, created_at, created_at, created_by_id from lnschema_core_legacy_transform",
-        "insert into lnschema_core_run (id, name, external_id, transform_id, created_at, run_at, created_by_id) select id, name, external_id, transform_id, created_at, created_at, created_by_id from lnschema_core_legacy_run",
-        "insert into lnschema_core_featureset (id, type, created_at, updated_at, created_by_id) select id, type, created_at, created_at, created_by_id from lnschema_core_legacy_featureset",
-        "insert into lnschema_core_file (id, description, suffix, size, hash, key, run_id, transform_id, storage_id, created_at, updated_at, created_by_id) select id, name, suffix, size, hash, key, run_id, transform_id, storage_id, created_at, created_at, created_by_id from lnschema_core_legacy_file",
-        # take into account the old file name convention
-        "insert into lnschema_core_file (id, name, suffix, size, hash, key, run_id, transform_id, storage_id, created_at, updated_at, created_by_id) select id, name, suffix, size, hash, key, run_id, transform_id, storage_id, created_at, created_at, created_by_id from lnschema_core_legacy_file",
-        "insert into lnschema_core_runinput (run_id, file_id) select run_id, file_id from lnschema_core_legacy_runinput",
-    ]
-    # fmt: on
-    with engine.connect() as conn:
-        for stmt in stmts:
-            try:
-                conn.execute(sa.text(stmt))
-                logger.success(stmt)
-            except Exception as e:
-                logger.warning(f"failed to execute: {stmt} because of {e}")
-
-
 # this bundles set up and migration management
 def setup_django(
     isettings: InstanceSettings,
@@ -167,9 +44,6 @@ def setup_django(
 ):
     if IS_RUN_FROM_IPYTHON:
         os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
-    if deploy_migrations:
-        check_legacy = check_is_legacy_instance_and_fix(isettings)
 
     import dj_database_url
     import django
@@ -213,8 +87,6 @@ def setup_django(
         if deploy_migrations:
             verbosity = 0 if init else 2
             call_command("migrate", verbosity=verbosity)
-            if check_legacy:
-                insert_legacy_data(isettings)
             if not init:
                 # only update if called from lamin migrate deploy
                 # if called from load_schema(..., init=True)
