c7d0f1fa64c4beddadec29e9b8c7ebd50fce21f3
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-31 02:13:38 +0200

    💚 Attempt fix

--- lamindb_setup/_register_instance.py
+++ lamindb_setup/_register_instance.py
@@ -15,9 +15,9 @@ def register():
         db=isettings.db if isettings.dialect != "sqlite" else None,
         schema=isettings._schema_str,
     )
-    if result == "instance-exists-already":
+    if result == "error-instance-exists-already":
         logger.info("instance was already registered")
-    elif isinstance(result, str):
-        raise RuntimeError(f"creating instance on hub failed:\n{result}")
+    elif result.startswith("error-"):
+        raise RuntimeError(f"Registering instance on hub failed:\n{result}")
     else:
         logger.save(f"instance registered: https://lamin.ai/{isettings.identifier}")
