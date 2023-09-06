fd00701e685418b3f41e5c6c89fc99f7205b873a
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-06 14:45:50 +0200

    💚 Fix

--- tests/hub/test_local.py
+++ tests/hub/test_local.py
@@ -5,5 +5,5 @@ from lamindb_setup.dev._hub_client import Settings, lamindb_client_config_settin
 
 def test_local():
     assert os.environ["LAMIN_ENV"] == "local"
-    prod_config = lamindb_client_config_settings()
+    prod_config = lamindb_client_config_settings(Settings())
     assert prod_config["supabase_api_url"] == Settings().supabase_api_url
