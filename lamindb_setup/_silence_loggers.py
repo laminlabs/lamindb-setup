6a4c52e67e4d21f4497969884e4981ef9faf8c23
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-06 12:18:47 +0200

    💚 Fix

--- lamindb_setup/_silence_loggers.py
+++ lamindb_setup/_silence_loggers.py
@@ -2,22 +2,18 @@ import logging
 
 
 def silence_loggers():
-    import botocore
+    from botocore import session
 
     # this gets logged 6 times
-    botocore.session.set_stream_logger(
-        name="botocore.credentials", level=logging.WARNING
-    )
-    botocore.session.set_stream_logger(name="botocore.hooks", level=logging.WARNING)
-    botocore.session.set_stream_logger(name="botocore.utils", level=logging.WARNING)
-    botocore.session.set_stream_logger(name="botocore.auth", level=logging.WARNING)
-    botocore.session.set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
+    session.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
+    session.set_stream_logger(name="botocore.hooks", level=logging.WARNING)
+    session.set_stream_logger(name="botocore.utils", level=logging.WARNING)
+    session.set_stream_logger(name="botocore.auth", level=logging.WARNING)
+    session.set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
     try:
         import aibotocore  # noqa
 
         # the 7th logging message of credentials came from aiobotocore
-        botocore.session.set_stream_logger(
-            name="aiobotocore.credentials", level=logging.WARNING
-        )
+        session.set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
     except ImportError:
         pass
