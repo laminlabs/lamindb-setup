a9866d6e81f90c239e2fa82fe877dd43a370e5ec
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-06 12:25:44 +0200

    💚 Fix

--- lamindb_setup/_silence_loggers.py
+++ lamindb_setup/_silence_loggers.py
@@ -1,19 +1,26 @@
 import logging
 
 
-def silence_loggers():
-    from botocore import session
+# from https://github.com/boto/boto3/blob/8c6e641bed8130a9d8cb4d97b4acbe7aa0d0657a/boto3/__init__.py#L37  # noqa
+def set_stream_logger(name, level):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    logger.addHandler(handler)
 
+
+def silence_loggers():
     # this gets logged 6 times
-    session.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
-    session.set_stream_logger(name="botocore.hooks", level=logging.WARNING)
-    session.set_stream_logger(name="botocore.utils", level=logging.WARNING)
-    session.set_stream_logger(name="botocore.auth", level=logging.WARNING)
-    session.set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
+    set_stream_logger(name="botocore.credentials", level=logging.WARNING)
+    set_stream_logger(name="botocore.hooks", level=logging.WARNING)
+    set_stream_logger(name="botocore.utils", level=logging.WARNING)
+    set_stream_logger(name="botocore.auth", level=logging.WARNING)
+    set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
     try:
         import aibotocore  # noqa
 
         # the 7th logging message of credentials came from aiobotocore
-        session.set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
+        set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
     except ImportError:
         pass
