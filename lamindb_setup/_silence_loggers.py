a07bc34cba59faaae9ea32703db379fd42bd71a4
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-06 12:01:10 +0200

    ✅ Test logger

--- lamindb_setup/_silence_loggers.py
+++ lamindb_setup/_silence_loggers.py
@@ -2,17 +2,22 @@ import logging
 
 
 def silence_loggers():
-    try:
-        import boto3
+    import botocore
 
-        # this gets logged 6 times
-        boto3.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
-        boto3.set_stream_logger(name="botocore.hooks", level=logging.WARNING)
-        boto3.set_stream_logger(name="botocore.utils", level=logging.WARNING)
-        boto3.set_stream_logger(name="botocore.auth", level=logging.WARNING)
-        boto3.set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
+    # this gets logged 6 times
+    botocore.session.set_stream_logger(
+        name="botocore.credentials", level=logging.WARNING
+    )
+    botocore.session.set_stream_logger(name="botocore.hooks", level=logging.WARNING)
+    botocore.session.set_stream_logger(name="botocore.utils", level=logging.WARNING)
+    botocore.session.set_stream_logger(name="botocore.auth", level=logging.WARNING)
+    botocore.session.set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
+    try:
+        import aibotocore  # noqa
+
         # the 7th logging message of credentials came from aiobotocore
-        boto3.set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
-        boto3.set_stream_logger(name="urllib3.connectionpool", level=logging.WARNING)
+        botocore.session.set_stream_logger(
+            name="aiobotocore.credentials", level=logging.WARNING
+        )
     except ImportError:
         pass
