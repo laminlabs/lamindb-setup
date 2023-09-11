d8bb12ba1a483f91bd83f460ae84bf18d1daf037
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-11 12:14:18 +0200

    ✅ Test django migrate

--- tests/prod-only/test_cli.py
+++ tests/prod-only/test_cli.py
@@ -4,3 +4,8 @@ import os
 def test_entrypoint():
     exit_status = os.system("lamin --help")
     assert exit_status == 0
+
+
+def test_migrate_create():
+    exit_status = os.system("lamin migrate create")
+    assert exit_status == 0
