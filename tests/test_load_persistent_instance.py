c6080ea4d7b70b2d621998d88d061beeae3ee3f9
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-20 13:44:27 +0200

    💚 Fix

--- tests/test_load_persistent_instance.py
+++ tests/test_load_persistent_instance.py
@@ -11,9 +11,7 @@ from lamindb_setup.dev._hub_crud import select_instance_by_owner_name
 
 def test_load_persistent_instance():
     assert ln_setup.dev.upath.AWS_CREDENTIALS_PRESENT is None
-    ln_setup.login(
-        email="static-testuser1@lamin.ai", password="static-testuser1-password"
-    )
+    ln_setup.login("static-testuser1@lamin.ai", password="static-testuser1-password")
     ln_setup.load("static-testuser1/static-testinstance1")
     hub = connect_hub_with_auth()
     instance = select_instance_by_owner_name(
