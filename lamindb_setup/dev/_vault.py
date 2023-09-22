e4b6b80d0ec0a99cc095e63d1f5876da042b2145
fredericenard <frederic.enard@gmail.com>
2023-09-22 03:20:49 -0400

    :mute: Remove print

--- lamindb_setup/dev/_vault.py
+++ lamindb_setup/dev/_vault.py
@@ -119,17 +119,6 @@ def init_instance_vault(
 ):
     vault_admin_client = create_vault_admin_client(instance_id)
 
-    print(
-        "Args",
-        instance_id,
-        admin_account_id,
-        db_host,
-        db_port,
-        db_name,
-        vault_db_username,
-        vault_db_password,
-    )
-
     create_or_update_connection_config_db(
         vault_admin_client=vault_admin_client,
         instance_id=instance_id,
