ec6414cc09deddccbdf721051b9110b487d76ea5
Alex Wolf <f.alexander.wolf@gmail.com>
2023-10-09 20:46:42 +0200

    🚸 Silence logger in CLI

--- lamindb_setup/__main__.py
+++ lamindb_setup/__main__.py
@@ -155,6 +155,9 @@ def main():
         )
     elif args.command == "init":
         from ._init_instance import init
+        from ._silence_loggers import silence_loggers
+
+        silence_loggers()
 
         return init(
             storage=args.storage,
@@ -175,6 +178,9 @@ def main():
         )
     elif args.command == "load":
         from ._load_instance import load
+        from ._silence_loggers import silence_loggers
+
+        silence_loggers()
 
         return load(
             identifier=args.instance,
