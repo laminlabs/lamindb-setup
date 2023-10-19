3d5693c1349004b1cb18ac97fc1a2e52f98dd98e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-10-19 22:24:26 +0200

    🔖 Release 0.55.5

--- lamindb_setup/__init__.py
+++ lamindb_setup/__init__.py
@@ -41,7 +41,7 @@ Developer API.
 """
 
 
-__version__ = "0.55.4"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.55.5"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import sys
 from os import name as _os_name
