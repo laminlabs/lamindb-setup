9a5dfc4ee693d452fa085b7da746e295869e657c
Alex Wolf <f.alexander.wolf@gmail.com>
2023-12-21 20:17:04 +0100

    🚸 Add backward error message

--- lamindb_setup/dev/_settings_instance.py
+++ lamindb_setup/dev/_settings_instance.py
@@ -268,6 +268,12 @@ class InstanceSettings:
         # Is the database available and initialized as LaminDB?
         # returns a tuple of status code and message
         if self.dialect == "sqlite" and not self._sqlite_file.exists():
+            legacy_file = self.storage.key_to_filepath(f"{self.id.hex}.lndb")
+            if legacy_file.exists():
+                raise RuntimeError(
+                    "The SQLite file has been renamed!\nPlease rename your SQLite file"
+                    f" {legacy_file} to {self._sqlite_file}"
+                )
             return False, "SQLite file does not exist"
         from lamindb_setup import settings  # to check user
         from .django import setup_django
