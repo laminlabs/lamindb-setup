029dd1d07781ba7feb5b4d0835542ed0e9978b7b
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-05 21:59:15 +0100

    🚚 Migrate foreign key constraint

--- lamindb_setup/dev/_hub_crud.py
+++ lamindb_setup/dev/_hub_crud.py
@@ -11,7 +11,7 @@ def select_instance_by_owner_name(
     try:
         data = (
             client.table("instance")
-            .select("*, account!inner!fk_instance_account_id_account(*)")
+            .select("*, account!inner!instance_account_id_28936e8f_fk_account_id(*)")
             .eq("account.handle", owner)
             .eq("name", name)
             .execute()
