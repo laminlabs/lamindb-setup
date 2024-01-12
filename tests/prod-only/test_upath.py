cacab00684f33b1327901c8b0fbe6bae7a9fe03f
Sergei Rybakov <koncopd@gmail.com>
2024-01-12 15:01:44 +0100

    🐛 Fix option inheritance in create_path and add tests (#617)
    fix and test create_path

---
+++ tests/prod-only/test_upath.py
@@ -0,0 +1,18 @@
+from lamindb_setup.dev.upath import UPath, create_path
+
+
+def test_create_path():
+    upath = UPath("s3://lndb-setup-ci/xyz/", default_fill_cache=False)
+    assert "default_fill_cache" in upath._kwargs
+
+    upath = create_path(upath)
+    # test option inheritance
+    assert "default_fill_cache" in upath._kwargs
+    # test cache_regions setting for s3
+    assert "cache_regions" in upath._kwargs
+    # test removal of training slash
+    assert upath.as_posix()[-1] != "/"
+    assert (
+        UPath("s3://lndb-setup-ci/xyz").as_posix()
+        == create_path("s3://lndb-setup-ci/xyz/").as_posix()
+    )
