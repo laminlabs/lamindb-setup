052b065ad6caa9a6275d007d3f1889dd161444f4
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-29 15:02:16 +0100

    🎨 Fix

--- lamindb_setup/_register_instance.py
+++ lamindb_setup/_register_instance.py
@@ -1,6 +1,3 @@
-from uuid import UUID
-
-from lamin_utils import logger
 from .dev.django import setup_django
 from ._settings import settings
 
@@ -15,19 +12,6 @@ def register(_test: bool = False):
     if not check_instance_setup() and not _test:
         setup_django(isettings)
 
-    result = init_instance_hub(
-        id=isettings.id,
-        name=isettings.name,
-        storage=isettings.storage,
-        db=isettings.db if isettings.dialect != "sqlite" else None,
-        schema=isettings._schema_str,
-    )
-    if not isinstance(result, UUID):
-        if result == "error-db-already-exists":
-            logger.warning("DB already exists")
-        else:
-            raise RuntimeError(f"Registering instance on hub failed:\n{result}")
-    else:
-        logger.save(f"instance registered: https://lamin.ai/{isettings.identifier}")
-        isettings._id = result
-        isettings._persist()
+    isettings = settings.instance
+    init_instance_hub(isettings)
+    isettings._persist()
