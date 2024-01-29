0629a8aa4351f045705c15cd700f26dc0ae7fa0f
Alex Wolf <f.alexander.wolf@gmail.com>
2024-01-29 14:37:01 +0100

    💚 Fix register calls

--- lamindb_setup/_register_instance.py
+++ lamindb_setup/_register_instance.py
@@ -5,14 +5,14 @@ from .dev.django import setup_django
 from ._settings import settings
 
 
-def register():
+def register(_test: bool = False):
     """Register an instance on the hub."""
     from .dev._hub_core import init_instance as init_instance_hub
     from ._check_instance_setup import check_instance_setup
 
     isettings = settings.instance
 
-    if not check_instance_setup():
+    if not check_instance_setup() and not _test:
         setup_django(isettings)
 
     result = init_instance_hub(
