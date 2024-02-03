f01b4a19f907977ea8ad8ada8eee56cad4a18bdb
Alex Wolf <f.alexander.wolf@gmail.com>
2024-02-03 23:48:15 +0100

    ♻️ Iterate on importdb and exportdb (#642)

--- lamindb_setup/_exportdb.py
+++ lamindb_setup/_exportdb.py
@@ -4,19 +4,18 @@ from importlib import import_module
 
 MODELS = {
     "core": {
-        "Dataset": False,
-        "File": False,
+        "Collection": False,
+        "Artifact": False,
         "Transform": False,
         "Run": True,
         "User": False,
         "Storage": False,
         "Feature": False,
         "FeatureSet": False,
-        "Modality": False,
         "ULabel": False,
     },
     "bionty": {
-        "Species": False,
+        "Organism": False,
         "Gene": False,
         "Protein": False,
         "CellMarker": False,
@@ -31,19 +30,22 @@ MODELS = {
         "Ethnicity": False,
         "PublicSource": False,
     },
-    "lamin1": {
-        "ExperimentType": False,
-        "Experiment": False,
-        "Well": False,
-        "TreatmentTarget": False,
-        "Treatment": False,
-        "Biosample": False,
-        "Techsample": False,
-    },
+    # "lamin1": {
+    #     "ExperimentType": False,
+    #     "Experiment": False,
+    #     "Well": False,
+    #     "TreatmentTarget": False,
+    #     "Treatment": False,
+    #     "Biosample": False,
+    #     "Techsample": False,
+    # },
 }
 
 
-def export_database(directory):
+def exportdb() -> None:
+    directory = Path("./lamindb_export/")
+    directory.mkdir(parents=True, exist_ok=True)
+    import pandas as pd
     import lamindb_setup as ln_setup
 
     def export_registry(registry, directory):
@@ -52,7 +54,7 @@ def export_database(directory):
         df.to_parquet(directory / f"{table_name}.parquet", compression=None)
 
     # export data to parquet files
-    print(f"\nExporting data to parquet files in: {directory}\n")
+    print(f"\nexporting data to parquet files in: {directory}\n")
     for schema_name, models in MODELS.items():
         for model_name in models.keys():
             schema_module = import_module(f"lnschema_{schema_name}")
@@ -62,15 +64,3 @@ def export_database(directory):
             for many_to_many_name in many_to_many_names:
                 link_orm = getattr(registry, many_to_many_name).through
                 export_registry(link_orm, directory)
-
-
-if __name__ == "__main__":
-    directory = Path("./lamindb_export/new/")
-    response = input(f"Will export database to {directory}. Ok? (y/n)")
-    if response != "y":
-        quit()
-    directory.mkdir(parents=True, exist_ok=True)
-    import pandas as pd
-    import lamindb as ln  # noqa
-
-    export_database(directory)
---
+++ lamindb_setup/_importdb.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from ._export import MODELS
+from importlib import import_module
+
+
+def import_registry(registry, directory, connection):
+    import pandas as pd
+
+    table_name = registry._meta.db_table
+    df = pd.read_parquet(directory / f"{table_name}.parquet")
+    old_foreign_key_columns = [
+        column for column in df.columns if column.endswith("_old")
+    ]
+    for column in old_foreign_key_columns:
+        df.drop(column, axis=1, inplace=True)
+    df.to_sql(table_name, connection, if_exists="append", index=False)
+
+
+def importdb() -> None:
+    # import data from parquet files
+    directory = Path("./lamindb_export/")
+    if directory.exists():
+        response = input(
+            f"\n\nDo you want to import registries from here: {directory}? (y/n)\n"
+        )
+        if response != "y":
+            return None
+    from sqlalchemy import create_engine
+    import lamindb_setup as ln_setup
+
+    engine = create_engine(ln_setup.settings.instance.db, echo=False)
+    with engine.begin() as connection:
+        if ln_setup.settings.instance.dialect == "postgresql":
+            connection.execute("SET CONSTRAINTS ALL DEFERRED;")
+        for schema_name, models in MODELS.items():
+            for model_name in models.keys():
+                schema_module = import_module(f"lnschema_{schema_name}")
+                registry = getattr(schema_module, model_name)
+                import_registry(registry, directory, connection)
+                many_to_many_names = [
+                    field.name for field in registry._meta.many_to_many
+                ]
+                for many_to_many_name in many_to_many_names:
+                    link_orm = getattr(registry, many_to_many_name).through
+                    import_registry(link_orm, directory, connection)
