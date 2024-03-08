3bd17c6ddf439512f276621c7846f7b11d40413a
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-08 07:55:05 +0100

    🔖 Release 0.66.0

--- lamindb_setup/__init__.py
+++ lamindb_setup/__init__.py
@@ -14,7 +14,6 @@ Setup:
    login
    logout
    init
-   connect
    load
    close
    delete
@@ -38,7 +37,7 @@ Modules & settings:
 
 """
 
-__version__ = "0.65.3"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "0.66.0"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import sys
 from os import name as _os_name
