8be9af0e757842e5eb0d16044c6d01237285c1aa
Bernardo Penteado <bernardopbf3@gmail.com>
2024-03-11 11:21:49 -0300

    :green_heart: Fix

--- tests/storage/test_storage_stats.py
+++ tests/storage/test_storage_stats.py
@@ -5,10 +5,10 @@ def test_get_stat_dir_s3():
     import re
 
     message = UPath(
-        "s3://lamindb-dev-datasets/iris_studies/study0_raw_images"
+        "s3://lamindb-dev-datasets/iris_studies/study0_raw_images", anon=True
     ).view_tree()
     num_objects_view_tree = re.search(r"(\d+) files", message).group(1)
     _, _, _, num_objects_stat = get_stat_dir_s3(
-        UPath("s3://lamindb-dev-datasets/iris_studies/study0_raw_images")
+        UPath("s3://lamindb-dev-datasets/iris_studies/study0_raw_images", anon=True)
     )
     assert num_objects_stat == int(num_objects_view_tree)
