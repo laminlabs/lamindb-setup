e026ba1be7268375124ce513d8c335b50b722593
Bernardo Penteado <bernardopbf3@gmail.com>
2024-03-15 14:21:49 -0300

    :green_heart: Fix

--- tests/hub-local/test_hosted_instance_deletion_gate.py
+++ tests/hub-local/test_hosted_instance_deletion_gate.py
@@ -4,7 +4,7 @@ from laminhub_rest.test.utils.instance import (
 )
 from lamindb_setup.core.upath import create_path
 from lamindb_setup.core._settings_storage import TOUCH_FILE_PATH
-from lamindb_setup.core._hub_core import delete_instance_storage
+from lamindb_setup.core._hub_core import delete_instance
 from lamindb_setup import settings
 import pytest
 
@@ -30,11 +30,11 @@ def test_hosted_instance_deletion_gate(run_id, s3_bucket):
     path.fs.touch(str(path / "new_file"))
     instance_slug = f"{settings.user.handle}/{test_instance.name}"
     with pytest.raises(ValueError):
-        delete_instance_storage(instance_slug)
+        delete_instance(instance_slug)
 
     # Make sure instance and storage deletion is possible with empty storage
     path.fs.rm(str(path / "new_file"))
-    delete_instance_storage(instance_slug)
+    delete_instance(instance_slug)
 
     # Clean up other assets
     delete_test_instance(test_instance)
