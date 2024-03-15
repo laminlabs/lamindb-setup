06dec170b58bd4ab40b26862157f29af7b31e4ef
Bernardo Penteado <bernardopbf3@gmail.com>
2024-03-15 11:54:54 -0300

    Revert ":truck: Rename delete_storage to delete_storage_record"
    This reverts commit 08ec1abad655421d8f321ae60a07eaba32d5cb8d.

--- lamindb_setup/core/_hub_core.py
+++ lamindb_setup/core/_hub_core.py
@@ -33,16 +33,16 @@ from ._hub_utils import (
 )
 
 
-def delete_storage_record(
+def delete_storage(
     storage_uuid: UUID,
 ) -> None:
     return call_with_fallback_auth(
-        _delete_storage_record,
+        _delete_storage,
         storage_uuid=storage_uuid,
     )
 
 
-def _delete_storage_record(storage_uuid: UUID, client: Client) -> None:
+def _delete_storage(storage_uuid: UUID, client: Client) -> None:
     if storage_uuid is None:
         return None
     logger.important(f"deleting storage {storage_uuid.hex}")
@@ -109,7 +109,7 @@ def delete_storage_instance(instance_identifier: str) -> None:
         instance_account.pop("account")
         instance = instance_account
         delete_instance(UUID(instance["id"]))
-        delete_storage_record(UUID(instance["storage_id"]))
+        delete_storage(UUID(instance["storage_id"]))
 
 
 def delete_instance(
