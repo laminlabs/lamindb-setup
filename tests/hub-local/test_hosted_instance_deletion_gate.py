9331dc16c29a17358fbba25b55827a18a028d07d
Bernardo Penteado <bernardopbf3@gmail.com>
2024-03-19 12:28:11 -0300

    :white_check_mark: Update tests

--- tests/hub-local/test_hosted_instance_deletion_gate.py
+++ tests/hub-local/test_hosted_instance_deletion_gate.py
@@ -1,40 +1,32 @@
-from laminhub_rest.test.utils.instance import (
-    create_hosted_test_instance,
-    delete_test_instance,
-)
-from lamindb_setup.core.upath import create_path, InstanceNotEmpty
+from lamindb_setup.core.upath import InstanceNotEmpty
 from lamindb_setup.core._settings_storage import IS_INITIALIZED_KEY
 from lamindb_setup.core._hub_core import delete_instance
 from lamindb_setup import settings
-from upath import UPath
+import lamindb_setup as ln_setup
 import pytest
 
 
-def test_hosted_instance_deletion_gate(run_id, hosted_s3_bucket):
-    test_instance = create_hosted_test_instance(
-        f"test_instance_{run_id}", hosted_s3_bucket.name
-    )
+def test_hosted_instance_deletion_gate():
+    ln_setup.init(name="my-hosted", storage="create-s3")
 
     # Make sure 0-byte file is touched upon storage initialization
-    path = create_path(test_instance.storage_root)
-    object_string_paths = path.fs.find(path.as_posix())
-    assert len(object_string_paths) == 1
+    root_upath = settings.storage.root
+    root_string = root_upath.as_posix()
+    object_string_paths = root_upath.fs.find(root_string)
     assert (
-        object_string_paths[0]
-        == f"{test_instance.storage_root.replace('s3://', '')}/{IS_INITIALIZED_KEY}"
+        f"{root_string.replace('s3://', '')}/{IS_INITIALIZED_KEY}"
+        in object_string_paths
     )
 
     # Make sure gating function blocks instance and storage deletion if
     # storage location is not empty
-    new_file = UPath(path / "new_file")
+    new_file = root_upath / "new_file"
     new_file.touch()
-    instance_slug = f"{settings.user.handle}/{test_instance.name}"
+    instance_slug = settings.instance.slug
     with pytest.raises(InstanceNotEmpty):
         delete_instance(instance_slug)
 
     # Make sure instance and storage deletion is possible with empty storage
     new_file.unlink()
+    ln_setup.delete("my-hosted", force=True)
     delete_instance(instance_slug)
-
-    # Clean up other assets
-    delete_test_instance(test_instance)
