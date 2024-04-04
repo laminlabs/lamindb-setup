7f97866d509e36ff731a5f9dc28de1ef89c58f50
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-04 22:13:23 +0200

    ♻️ Replace TypeVar with TypeAlias (#707)
    * ♻️ Replace TypeVar with TypeAlias
    
    * 💚 Fix
    
    * ♻️ From future

--- lamindb_setup/core/types.py
+++ lamindb_setup/core/types.py
@@ -1,7 +1,7 @@
-from typing import TypeVar
+from __future__ import annotations
 from pathlib import Path
 
 # UPath is subclass of Path, hence, it's not necessary to list UPath
 # we keep it in the name of the TypeVar to make it clear to users that
 # cloud paths are allowed / PathStr is often associated with local paths
-UPathStr = TypeVar("UPathStr", str, Path)
+UPathStr = str | Path  # typing.TypeAlias, >3.10 on but already deprecated
