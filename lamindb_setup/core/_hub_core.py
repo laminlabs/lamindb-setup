0e0624a6051901f93e77924aa5a000c2df9b54f8
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-27 14:52:27 +0200

    ♻️ Fix auth token more

--- lamindb_setup/core/_hub_core.py
+++ lamindb_setup/core/_hub_core.py
@@ -131,7 +131,11 @@ def _delete_instance(
     if require_empty:
         root_string = instance_with_storage["storage"]["root"]
         # gate storage and instance deletion on empty storage location for
-        root_path = create_path(root_string, client.auth.get_session().access_token)
+        if client.auth.get_session() is not None:
+            access_token = client.auth.get_session().access_token
+        else:
+            access_token = None
+        root_path = create_path(root_string, access_token)
         mark_storage_root(
             root_path, instance_with_storage["storage"]["lnid"]
         )  # address permission error
