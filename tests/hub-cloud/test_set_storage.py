8fb3576e952d8d9067cfe47f4edbb89b0bd95632
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-29 12:32:38 +0200

    💚 Fix

--- tests/hub-cloud/test_set_storage.py
+++ tests/hub-cloud/test_set_storage.py
@@ -1,11 +1,11 @@
-from __future__ import annotations
-
 import lamindb_setup as ln_setup
+import pytest
 from lamindb_setup._add_remote_storage import add_managed_storage
 
 
 def test_set_storage_sqlite():
     ln_setup.delete("mydata", force=True)
     ln_setup.init(storage="./mydata", _test=True)
-    assert add_managed_storage("mydata2") == "set-storage-failed"
+    with pytest.raises(ValueError):
+        add_managed_storage("mydata2")
     ln_setup.delete("mydata", force=True)
