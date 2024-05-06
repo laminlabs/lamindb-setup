4df2d1d32d430b626dc32b50b52d727e1a1065f7
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-06 12:07:30 +0200

    ♻️ Actually use local db (#751)
    * ♻️ Actually use local db
    
    * 🚚 Rename error flag

--- lamindb_setup/_init_instance.py
+++ lamindb_setup/_init_instance.py
@@ -184,7 +184,7 @@ def validate_init_args(
     name_str = infer_instance_name(storage=storage, name=name, db=db)
     # test whether instance exists by trying to load it
     instance_slug = f"{settings.user.handle}/{name_str}"
-    response = connect(instance_slug, _raise_not_reachable_error=False, _test=_test)
+    response = connect(instance_slug, db=db, _raise_not_found_error=False, _test=_test)
     instance_state: Literal[
         "connected",
         "instance-corrupted-or-deleted",
