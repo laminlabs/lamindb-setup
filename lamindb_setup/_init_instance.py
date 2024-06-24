67b0fe15bc94975505e01af0761e3fb1bebe5062
Alex Wolf <f.alexander.wolf@gmail.com>
2024-06-24 12:53:43 +0200

    ♻️ Save schema to hub upon init (#782)
    * ♻️ Save schema to hub upon init
    
    * 💚 Fix
    
    * 💚 Fix
    
    * ♻️ Only call if registered on hub

--- lamindb_setup/_init_instance.py
+++ lamindb_setup/_init_instance.py
@@ -255,7 +255,10 @@ def init(
             schema=schema,
             uid=ssettings.uid,
         )
-        if isettings.is_remote and instance_state != "instance-corrupted-or-deleted":
+        register_on_hub = (
+            isettings.is_remote and instance_state != "instance-corrupted-or-deleted"
+        )
+        if register_on_hub:
             init_instance_hub(isettings)
         validate_sqlite_state(isettings)
         isettings._persist()
@@ -269,8 +272,10 @@ def init(
                 "locked instance (to unlock and push changes to the cloud SQLite file,"
                 " call: lamin close)"
             )
-        # we can debate whether this is the right setting, but this is how
-        # things have been and we'd like to not easily break backward compat
+        if register_on_hub and isettings.dialect != "sqlite":
+            from ._schema_metadata import update_schema_in_hub
+
+            update_schema_in_hub()
         settings.auto_connect = True
     except Exception as e:
         from ._delete import delete_by_isettings
