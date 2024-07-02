0f15e74bdea08fd3b0c2909b34f66ab15e1b4aaf
Frédéric Enard <frederic.enard@gmail.com>
2024-07-02 23:46:02 +0200

    Remove useless import

--- lamindb_setup/_schema_metadata.py
+++ lamindb_setup/_schema_metadata.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import hashlib
 import importlib
 import json
-from dataclasses import dataclass
 from typing import TYPE_CHECKING, Literal
 from uuid import UUID
 
