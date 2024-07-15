a3e6d9b22056c2626cc6244b78fe2145098fa30a
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-15 15:40:14 +0200

    ♻️ Refactor check for being clean

--- tests/hub-prod/test_global_settings.py
+++ tests/hub-prod/test_global_settings.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 
 import lamindb_setup as ln_setup
+from lamindb_setup.core.hashing import hash_dir
 
 
 def test_auto_connect():
@@ -32,13 +33,22 @@ def is_repo_clean() -> bool:
 
 
 def test_private_django_api():
+    from django import db
+
+    django_dir = Path(db.__file__).parent.parent
+
+    orig_size, orig_hash, _, _ = hash_dir(django_dir)
     current_state = ln_setup.settings.private_django_api
     ln_setup.settings.private_django_api = True
     # do not run below on CI, but only locally
     # installing django via git didn't succeed
     # assert not is_repo_clean()
+    _, hash, _, _ = hash_dir(django_dir)
+    assert hash != orig_hash
     assert ln_setup.settings._private_django_api_path.exists()
     ln_setup.settings.private_django_api = False
     # assert is_repo_clean()
+    _, hash, _, _ = hash_dir(django_dir)
+    assert hash == orig_hash
     assert not ln_setup.settings._private_django_api_path.exists()
     ln_setup.settings.private_django_api = current_state
