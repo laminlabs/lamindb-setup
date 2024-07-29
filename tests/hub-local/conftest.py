2ca01033e82c20551cb08632b002e2c4921df8c8
Frédéric Enard <frederic.enard@gmail.com>
2024-07-29 08:57:04 +0400

    :recycle: Refactor local hub conftest

--- tests/hub-local/conftest.py
+++ tests/hub-local/conftest.py
@@ -2,22 +2,28 @@ from __future__ import annotations
 
 import os
 
-from lamin_utils import logger
-from laminhub_rest.dev._setup_laminapp_rest import setup_local
+from laminhub_rest.dev._seed import seed_local_test
+from laminhub_rest.dev._supabase import SupabaseResources
+from laminhub_rest.dev._utils import remove_lamin_local_settings
+
+supabase_resources = SupabaseResources()
+
 
 pytest_plugins = [
     "laminhub_rest.core.account.user.test.fixtures",
     "laminhub_rest.test.fixtures.run_id",
 ]
 
-local_setup_state = setup_local()
-
 
 def pytest_configure():
-    assert os.environ["LAMIN_ENV"] == "local"
-    local_setup_state.__enter__()
+    os.environ["LAMIN_ENV"] = "local"
+    remove_lamin_local_settings()
+    supabase_resources.start_local()
+    supabase_resources.reset_local()
+    supabase_resources.migrate()
+    seed_local_test()
 
 
 def pytest_unconfigure():
-    if os.environ["LAMIN_ENV"] == "local":
-        local_setup_state.__exit__(None, None, None)
+    if supabase_resources.edge_function_process:
+        supabase_resources.start_local_edge_functions()
