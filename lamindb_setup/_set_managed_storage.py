28a59030651cb65107b5bc16fd3a8124212b8124
Koncopd <koncopd@gmail.com>
2024-08-15 12:32:55 +0200

    Safer set_managed_storage

--- lamindb_setup/_set_managed_storage.py
+++ lamindb_setup/_set_managed_storage.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 from lamin_utils import logger
 
 from ._init_instance import register_storage_in_instance
+from .core._hub_core import delete_storage_record
+from .core._hub_core import init_storage as init_storage_hub
 from .core._settings import settings
 from .core._settings_storage import init_storage
 
@@ -28,14 +30,28 @@ def set_managed_storage(root: UPathStr, **fs_kwargs):
         raise ValueError(
             "Can't add additional managed storage locations for instances that aren't managed through the hub."
         )
+
     ssettings = init_storage(
-        root=root, instance_id=settings.instance._id, register_hub=True
+        root=root, instance_id=settings.instance._id, prevent_register_hub=True
     )
     if ssettings._instance_id is None:
         raise ValueError(
             f"Cannot manage storage without write access: {ssettings.root}"
         )
+
+    # this stores the result of init_storage_hub
+    hub_record_status: Literal["hub_record_retrieved", "hub_record_created"] | None = (
+        None
+    )
+    if settings.instance.is_on_hub:
+        hub_record_status = init_storage_hub(ssettings, auto_populate_instance=True)
+    try:
+        register_storage_in_instance(ssettings)
+    except Exception as e:
+        if hub_record_status == "hub_record_created":
+            delete_storage_record(ssettings._uuid)  # type: ignore
+        raise e
+
     settings.instance._storage = ssettings
     settings.instance._persist()  # this also updates the settings object
-    register_storage_in_instance(ssettings)
     settings.storage._set_fs_kwargs(**fs_kwargs)
