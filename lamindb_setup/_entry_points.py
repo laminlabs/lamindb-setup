07d45f89cb392326278ff44f5a594b6c3d50f734
Andreas Poehlmann <ap--@users.noreply.github.com>
2024-09-05 17:25:47 +0200

    load lamindb_setup.in_import entry points and call them

--- lamindb_setup/_entry_points.py
+++ lamindb_setup/_entry_points.py
@@ -4,7 +4,7 @@ from importlib.metadata import entry_points
 
 
 def call_registered_entry_points(group, **kwargs):
-    """load and call entry points registered under group"""
+    """load and call entry points registered under group."""
     if sys.version_info >= (3, 10):
         eps = entry_points(group=group)
     else:
