7516f3c20e2d20b70f825b2d331cd66499375d37
Koncopd <koncopd@gmail.com>
2024-10-21 10:55:05 +0200

    edit test name

--- tests/hub-prod/test_aws_credentials_manager.py
+++ tests/hub-prod/test_aws_credentials_manager.py
@@ -2,7 +2,7 @@ from lamindb_setup.core._aws_credentials import get_aws_credentials_manager
 from lamindb_setup.core.upath import UPath
 
 
-def test_anon_piblic():
+def test_anon_public():
     aws_credentials_manager = get_aws_credentials_manager()
     assert aws_credentials_manager.anon_public is not None
     assert not aws_credentials_manager.anon_public
