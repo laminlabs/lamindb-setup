7302e5312777749e2b028a4e236918d34630a6fb
Koncopd <koncopd@gmail.com>
2025-01-16 13:11:07 +0100

    ignore formatting for now

--- lamindb_setup/core/_settings_save.py
+++ lamindb_setup/core/_settings_save.py
@@ -40,9 +40,9 @@ def save_settings(
 ):
     with open(settings_file, "w") as f:
         for store_key, type_ in type_hints.items():
-            if type_ == Optional[str]:
+            if type_ == Optional[str]:  # noqa: UP007
                 type_ = str
-            if type_ == Optional[bool]:
+            if type_ == Optional[bool]:  # noqa: UP007
                 type_ = bool
             if "__" not in store_key:
                 if store_key == "model_config":
