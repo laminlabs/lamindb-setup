c3d9c23c1b644e88fbc7979855aee13e4b5ad93e
Sergei Rybakov <koncopd@gmail.com>
2025-02-26 18:33:03 +0100

    ✨ Support R2 (#979)
    * support R2
    
    * test

--- tests/hub-prod/test_upath.py
+++ tests/hub-prod/test_upath.py
@@ -56,6 +56,10 @@ def test_create_path():
     upath = create_path("http://some_url.com/")
     assert upath.storage_options["use_listings_cache"]
     assert "timeout" in upath.storage_options["client_kwargs"]
+    # test R2
+    upath = create_path("s3://bucket/key?endpoint_url=https://r2.cloudflarestorage.com")
+    assert upath.as_posix() == "s3://bucket/key"
+    assert upath.storage_options["fixed_upload_size"]
 
 
 def test_progress_callback_size():
