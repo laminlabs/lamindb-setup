81333550a073c7bbb9201619c1db5eaac5de16b2
Koncopd <koncopd@gmail.com>
2025-03-14 17:21:42 +0100

    check token renewal

--- tests/hub-local/scripts/script-connect-fine-grained-access.py
+++ tests/hub-local/scripts/script-connect-fine-grained-access.py
@@ -3,6 +3,9 @@ import os
 import lamindb_setup as ln_setup
 
 assert os.environ["LAMIN_ENV"] == "local"
+# check token renewal in _hub_client.request_get_auth
+invalid_token = "header1.payload1.signature1"
+ln_setup.settings.user.access_token = invalid_token
 
 ln_setup.connect("instance_access_v2")
 
