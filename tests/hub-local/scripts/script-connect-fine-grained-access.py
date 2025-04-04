97fd96e1fdc0201a398ddac9ace7a9534ed463fb
Koncopd <koncopd@gmail.com>
2025-04-04 23:50:27 +0200

    allow calling access_db with a dict

--- tests/hub-local/scripts/script-connect-fine-grained-access.py
+++ tests/hub-local/scripts/script-connect-fine-grained-access.py
@@ -8,13 +8,23 @@ assert os.environ["LAMIN_ENV"] == "local"
 
 ln_setup.connect("instance_access_v2")
 
-assert ln_setup.settings.instance._fine_grained_access
-assert ln_setup.settings.instance._db_permissions == "jwt"
-assert ln_setup.settings.instance._api_url is not None
+isettings = ln_setup.settings.instance
 
+assert isettings._fine_grained_access
+assert isettings._db_permissions == "jwt"
+assert isettings._api_url is not None
+
+# check calling access_db with a dict
+instance_dict = {
+    "owner": isettings.owner,
+    "name": isettings.name,
+    "id": isettings._id,
+    "api_url": isettings._api_url,
+}
+access_db(instance_dict)
 # check calling access_db with anonymous user
 ln_setup.settings.user.handle = "anonymous"
 with pytest.raises(RuntimeError):
-    access_db(ln_setup.settings.instance)
+    access_db(isettings)
 # check with providing access_token explicitly
-access_db(ln_setup.settings.instance, ln_setup.settings.user.access_token)
+access_db(isettings, ln_setup.settings.user.access_token)
