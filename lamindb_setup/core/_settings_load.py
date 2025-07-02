4656558bb88a49c8777c2215646523f3afd83a51
Koncopd <koncopd@gmail.com>
2025-07-02 10:46:10 +0200

    reorder priorities

--- lamindb_setup/core/_settings_load.py
+++ lamindb_setup/core/_settings_load.py
@@ -26,20 +26,17 @@ from ._settings_user import UserSettings
 
 def load_cache_path_from_settings(storage_settings: Path | None = None) -> Path | None:
     if storage_settings is None:
-        system_settings = system_settings_file()
-        if system_settings.exists():
-            cache_path = dotenv_values(system_settings).get("lamindb_cache_path", None)
-        else:
-            cache_path = None
-
-        if cache_path in {None, "null"}:
-            storage_settings = platform_user_storage_settings_file()
+        paltform_user_storage_settings = platform_user_storage_settings_file()
+        if paltform_user_storage_settings.exists():
+            cache_path = dotenv_values(storage_settings).get("lamindb_cache_path", None)
+        if cache_path in {None, "null", ""}:
+            storage_settings = system_settings_file()
         else:
             return Path(cache_path)
 
     if storage_settings.exists():
         cache_path = dotenv_values(storage_settings).get("lamindb_cache_path", None)
-        return Path(cache_path) if cache_path not in {None, "null"} else None
+        return Path(cache_path) if cache_path not in {None, "null", ""} else None
     else:
         return None
 
