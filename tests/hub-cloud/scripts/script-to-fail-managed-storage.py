5b3b2a0908b05bbc896d71f8a2984f54b1db55c1
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-02 11:58:39 +0200

    💚 Fix

--- tests/hub-cloud/scripts/script-to-fail-managed-storage.py
+++ tests/hub-cloud/scripts/script-to-fail-managed-storage.py
@@ -7,7 +7,7 @@ from lamindb_setup._set_managed_storage import set_managed_storage
 from lamindb_setup.core._hub_client import connect_hub_with_auth
 
 # a user should have read-only access to laminlabs/lamin-site-assets
-ln_setup.login("testuser1")
+ln_setup.login("testuser2")
 ln_setup.connect("laminlabs/lamin-site-assets")
 
 test_root = Path("./test_script_ci_storage").resolve().as_posix()
