1a8bd95ba66dd99a68fa467578dccae9544b38da
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-07 01:37:51 +0200

    ♻️ Longer uids for branch and space (#1081)
    * ♻️ Longer uids for branch and space
    
    * 💚 Fix
    
    * ♻️ Better
    
    * 💚 Fix

--- tests/hub-prod/test_global_settings.py
+++ tests/hub-prod/test_global_settings.py
@@ -21,11 +21,11 @@ def test_branch():
     import lamindb as ln
 
     ln_setup.settings._branch_path.unlink(missing_ok=True)
-    assert ln_setup.settings.branch.uid == "m"
+    assert ln_setup.settings.branch.uid == 12 * "m"
     ln_setup.settings.branch = "archive"
-    assert ln_setup.settings._branch_path.read_text() == "a"
+    assert ln_setup.settings._branch_path.read_text() == 12 * "a"
     ln_setup.settings.branch = "main"
-    assert ln_setup.settings._branch_path.read_text() == "m"
+    assert ln_setup.settings._branch_path.read_text() == 12 * "m"
     with pytest.raises(ln.errors.DoesNotExist):
         ln_setup.settings.branch = "not_exists"
 
@@ -34,9 +34,9 @@ def test_space():
     import lamindb as ln
 
     ln_setup.settings._space_path.unlink(missing_ok=True)
-    assert ln_setup.settings.space.uid == "a"
+    assert ln_setup.settings.space.uid == 12 * "a"
     ln_setup.settings.space = "all"
-    assert ln_setup.settings._space_path.read_text() == "a"
+    assert ln_setup.settings._space_path.read_text() == 12 * "a"
     with pytest.raises(ln.errors.DoesNotExist):
         ln_setup.settings.space = "not_exists"
 
