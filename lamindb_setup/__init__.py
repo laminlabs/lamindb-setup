c44fe10c216895f082077e6b11a0eacdd16db0bc
Alex Wolf <f.alexander.wolf@gmail.com>
2025-09-23 14:54:24 +0200

    🔖 Release 1.10.2

--- lamindb_setup/__init__.py
+++ lamindb_setup/__init__.py
@@ -35,7 +35,7 @@ Modules & settings:
 
 """
 
-__version__ = "1.10.1"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "1.10.2"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os
 
