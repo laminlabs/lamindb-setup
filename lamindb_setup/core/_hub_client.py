fe45d9ef0a6143093ec9c4d432dc6dc993bac8a6
Koncopd <koncopd@gmail.com>
2025-10-07 15:58:04 +0200

    use httpx_retries

--- lamindb_setup/core/_hub_client.py
+++ lamindb_setup/core/_hub_client.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import json
 import os
+from contextlib import contextmanager
 from typing import Literal
 from urllib.request import urlretrieve
 
-from httpx import HTTPTransport
+import httpx
+from httpx_retries import Retry, RetryTransport
 from lamin_utils import logger
 from pydantic_settings import BaseSettings
 from supabase import Client, create_client  # type: ignore
@@ -61,7 +63,7 @@ class Environment:
         self.supabase_anon_key: str = key
 
 
-DEFAULT_TIMEOUT = 20
+DEFAULT_TIMEOUT = 10
 
 
 # runs ~0.5s
@@ -78,11 +80,17 @@ def connect_hub(
     client = create_client(env.supabase_api_url, env.supabase_anon_key, client_options)
     # needed to enable retries for http requests in supabase
     # these are separate clients and need separate transports
-    # retries are done only in case an httpx.ConnectError or an httpx.ConnectTimeout occurs
-    transport_kwargs = {"verify": True, "http2": True, "retries": 2}
-    client.auth._http_client._transport = HTTPTransport(**transport_kwargs)
-    client.functions._client._transport = HTTPTransport(**transport_kwargs)
-    client.postgrest.session._transport = HTTPTransport(**transport_kwargs)
+    transports = []
+    for _ in range(3):
+        transports.append(
+            RetryTransport(
+                retry=Retry(total=2, backoff_factor=0.2),
+                transport=httpx.HTTPTransport(verify=True, http2=True),
+            )
+        )
+    client.auth._http_client._transport = transports[0]
+    client.functions._client._transport = transports[1]
+    client.postgrest.session._transport = transports[2]
     return client
 
 
@@ -199,17 +207,26 @@ def call_with_fallback(
     return result
 
 
-def requests_client():
-    # local is used in tests
-    if os.environ.get("LAMIN_ENV", "prod") == "local":
-        from fastapi.testclient import TestClient
-        from laminhub_rest.main import app
+@contextmanager
+def httpx_client():
+    client = None
+    try:
+        # local is used in tests
+        if os.environ.get("LAMIN_ENV", "prod") == "local":
+            from fastapi.testclient import TestClient
+            from laminhub_rest.main import app
 
-        return TestClient(app)
-
-    import requests  # type: ignore
-
-    return requests
+            client = TestClient(app)
+        else:
+            transport = RetryTransport(
+                retry=Retry(total=2, backoff_factor=0.2),
+                transport=httpx.HTTPTransport(verify=True, http2=True),
+            )
+            client = httpx.Client(transport=transport)
+        yield client
+    finally:
+        if client is not None:
+            client.close()
 
 
 def request_with_auth(
@@ -219,15 +236,15 @@ def request_with_auth(
     renew_token: bool = True,
     **kwargs,
 ):
-    requests = requests_client()
-
     headers = kwargs.pop("headers", {})
     headers["Authorization"] = f"Bearer {access_token}"
-
-    make_request = getattr(requests, method)
     timeout = kwargs.pop("timeout", DEFAULT_TIMEOUT)
 
-    response = make_request(url, headers=headers, timeout=timeout, **kwargs)
+    with httpx_client() as client:
+        response = getattr(client, method)(
+            url, headers=headers, timeout=timeout, **kwargs
+        )
+
     status_code = response.status_code
     # update access_token and try again if failed
     if not (200 <= status_code < 300) and renew_token:
@@ -244,5 +261,8 @@ def request_with_auth(
 
         headers["Authorization"] = f"Bearer {access_token}"
 
-        response = make_request(url, headers=headers, timeout=timeout, **kwargs)
+        with httpx_client() as client:
+            response = getattr(client, method)(
+                url, headers=headers, timeout=timeout, **kwargs
+            )
     return response
