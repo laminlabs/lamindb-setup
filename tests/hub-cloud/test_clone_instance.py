f053bacffb03ee9a7698f78531b82add5b5dba50
Lukas Heumos <lukas.heumos@posteo.net>
2025-10-08 17:55:28 +0200

    🎨 New structure
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/hub-cloud/test_clone_instance.py
+++ tests/hub-cloud/test_clone_instance.py
@@ -1,31 +1,51 @@
+import shutil
 import sqlite3
+from subprocess import DEVNULL, run
 from urllib.parse import urlparse
 
-import lamindb_setup as setup
+import lamindb_setup as ln_setup
 import pandas as pd
 import pytest
 from django.db import connection
-
-# To run these tests locally, you need to have the local backend set up: https://github.com/laminlabs/laminhub?tab=readme-ov-file#local-backend
-# Ensure that rest-hub is installed in editable mode
-# It might be required to use a specific supabase version. Currently 2.31.8 works and the latest version 2.48.3 does not.
+from laminci.db import setup_local_test_postgres
 
 
-def test_init_clone_successful(create_instance_fine_grained_access):
-    instance = create_instance_fine_grained_access
+@pytest.fixture
+def local_postgres_instance():
+    pgurl = setup_local_test_postgres()
+    ln_setup.init(
+        storage="./test-postgres-local",
+        modules="bionty",
+        name="test-postgres-local",
+        db=pgurl,
+    )
+
+    yield
+
+    try:
+        shutil.rmtree("./test-postgres-local")
+        ln_setup.delete("test-postgres-local", force=True)
+    except Exception as e:
+        print(e)
+    run("docker stop pgtest && docker rm pgtest", shell=True, stdout=DEVNULL)
+
+
+def test_init_clone_successful(local_postgres_instance):
     print("CLONE TESTING")
 
-    setup.connect(f"testadmin1/{instance.name}")
+    ln_setup.connect("test-local-postgres")
 
     original_tables = pd.read_sql(
-        "SELECT tablename as name FROM pg_tables WHERE schemaname='public'", connection
+        "SELECT tablename as name FROM pg_tables WHERE schemaname='public'",
+        ln_setup.settings.instance.db,
     )
     print(original_tables)
+    """
 
-    setup.core.init_clone(f"testadmin1/{instance.name}")
+    ln_setup.core.init_clone(f"testadmin1/{local_postgres_instance.name}")
 
     # needs to be adapted when we don't connect anymore
-    db_uri = setup.settings.instance.db
+    db_uri = ln_setup.settings.instance.db
     db_path = (
         urlparse(db_uri).path
         if db_uri.startswith("sqlite:///")
@@ -40,8 +60,8 @@ def test_init_clone_successful(create_instance_fine_grained_access):
 
     print(clone_tables)
 
-    setup.disconnect()
-    """
+    ln_setup.disconnect()
+
 
 
 
