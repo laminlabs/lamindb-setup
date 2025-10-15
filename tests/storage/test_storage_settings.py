bf4511f965eca025a2d1b0191b343f1c8a466d18
Koncopd <koncopd@gmail.com>
2025-10-15 18:14:01 +0200

    test incorrect region

--- tests/storage/test_storage_settings.py
+++ tests/storage/test_storage_settings.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+import pytest
 from lamindb_setup import login, logout
 from lamindb_setup.core._hub_core import get_default_bucket_for_instance
 from lamindb_setup.core._settings_storage import StorageSettings
@@ -30,6 +31,11 @@ def test_endpoint_url():
 
 def test_get_default_bucket_for_instance():
     lamindata_id = UUID("037ba1e0-8d80-4f91-a902-75a47735076a")
+
+    # test incorrect region
+    with pytest.raises(ValueError) as e:
+        get_default_bucket_for_instance(None, region="wrong_region")
+    assert "region has to be one of" in str(e)
 
     assert get_default_bucket_for_instance(lamindata_id) == "s3://lamin-us-east-1"
 
