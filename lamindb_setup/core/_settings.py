069ee57f2391e5701a5310e8dfedd8842b3c98a2
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-21 14:07:20 +0200

    🚸 Prettier instance settings

--- lamindb_setup/core/_settings.py
+++ lamindb_setup/core/_settings.py
@@ -326,20 +326,20 @@ class SetupSettings:
             return object.__repr__(self)
         repr = ""
         if self._instance_exists:
-            repr += "Current branch & space:\n"
+            instance_rep = self.instance.__repr__().split("\n")
+            repr += f"{instance_rep[0]}\n"
+            repr += f" - work-dir: {self.work_dir}\n"
             repr += f" - branch: {self._read_branch_idlike_name()[1]}\n"
-            repr += f" - space: {self._read_space_idlike_name()[1]}\n"
-            repr += self.instance.__repr__()
+            repr += f" - space: {self._read_space_idlike_name()[1]}"
+            repr += "\nDetails:\n"
+            repr += "\n".join(instance_rep[1:])
         else:
-            repr += "Current instance: None"
-        repr += "\nConfig:\n"
-        repr += f" - private Django API: {self.private_django_api}\n"
-        repr += "Local directories:\n"
-        repr += f" - working directory: {self.work_dir}\n"
+            repr += "Instance: None"
+        repr += "\nCache & settings:\n"
         repr += f" - cache: {self.cache_dir.as_posix()}\n"
         repr += f" - user settings: {settings_dir.as_posix()}\n"
-        repr += f" - system settings: {system_settings_dir.as_posix()}\n"
-        repr += self.user.__repr__()
+        repr += f" - system settings: {system_settings_dir.as_posix()}"
+        repr += f"\nUser: {self.user.handle}"
         return repr
 
 
