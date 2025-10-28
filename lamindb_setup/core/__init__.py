a39e8b40863f50fc85929f5b39e7c0ddfe5833b3
Alex Wolf <f.alexander.wolf@gmail.com>
2025-10-28 09:07:50 +0100

    ♻️ Simplify

--- lamindb_setup/core/__init__.py
+++ lamindb_setup/core/__init__.py
@@ -1,21 +1,16 @@
 """Core setup library.
 
-Settings:
-
-.. autosummary::
-   :toctree:
-
-   SetupSettings
-   UserSettings
-   InstanceSettings
-   StorageSettings
+.. autoclass:: SetupSettings
+.. autoclass:: UserSettings
+.. autoclass:: InstanceSettings
+.. autoclass:: StorageSettings
 
 """
 
 from . import django, upath
 from ._clone import connect_local_sqlite, init_local_sqlite
-from ._deprecated import deprecated
-from ._docs import doc_args
+from ._deprecated import deprecated  # documented in lamindb.base
+from ._docs import doc_args  # documented in lamindb.base
 from ._settings import SetupSettings
 from ._settings_instance import InstanceSettings
 from ._settings_storage import StorageSettings
