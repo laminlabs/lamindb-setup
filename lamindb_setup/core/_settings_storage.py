b2c0d0beedc8764511c981a6e1e155e406c441c8
Alex Wolf <f.alexander.wolf@gmail.com>
2025-11-13 09:38:06 +0100

    ♻️ Refactor storage settings

--- lamindb_setup/core/_settings_storage.py
+++ lamindb_setup/core/_settings_storage.py
@@ -12,17 +12,14 @@ from lamin_utils import logger
 from lamindb_setup.errors import StorageAlreadyManaged
 
 from ._aws_options import (
-    HOSTED_REGIONS,
     LAMIN_ENDPOINTS,
     get_aws_options_manager,
 )
-from ._aws_storage import find_closest_aws_region
 from ._deprecated import deprecated
 from .hashing import hash_and_encode_as_b62
 from .upath import (
     LocalPathClasses,
     UPath,
-    _split_path_query,
     create_path,
     get_storage_region,
 )
@@ -56,6 +53,17 @@ def get_storage_type(root_as_str: str) -> StorageType:
     # init_storage checks that the root protocol belongs to VALID_PROTOCOLS
     protocol = fsspec.utils.get_protocol(root_as_str)
     return convert.get(protocol, protocol)  # type: ignore
+
+
+def convert_root_path_to_str(root: UPath) -> str:
+    """Format a root path string."""
+    # embed endpoint_url into path string for storing and displaying
+    if root.protocol == "s3":
+        endpoint_url = root.storage_options.get("endpoint_url", None)
+        # LAMIN_ENDPOINTS include None
+        if endpoint_url not in LAMIN_ENDPOINTS:
+            return f"s3://{root.path.rstrip('/')}?endpoint_url={endpoint_url}"
+    return root.as_posix().rstrip("/")
 
 
 def mark_storage_root(
@@ -336,13 +344,7 @@ class StorageSettings:
     @property
     def root_as_str(self) -> str:
         """Formatted root string."""
-        # embed endpoint_url into path string for storing and displaying
-        if self._root_init.protocol == "s3":
-            endpoint_url = self._root_init.storage_options.get("endpoint_url", None)
-            # LAMIN_ENDPOINTS include None
-            if endpoint_url not in LAMIN_ENDPOINTS:
-                return f"s3://{self._root_init.path.rstrip('/')}?endpoint_url={endpoint_url}"
-        return self._root_init.as_posix().rstrip("/")
+        return convert_root_path_to_str(self._root_init)
 
     @property
     def cache_dir(
