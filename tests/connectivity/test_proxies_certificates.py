ace5220b8ac528a46b5c3787d6289b97d28f4521
Koncopd <koncopd@gmail.com>
2025-11-21 13:48:25 +0200

    test setup

--- tests/connectivity/test_proxies_certificates.py
+++ tests/connectivity/test_proxies_certificates.py
@@ -1,7 +1,17 @@
 import os
+import socket
 
 
-def test_env():
-    assert "HTTPS_PROXY" in os.environ
-    assert "HTTP_PROXY" in os.environ
-    assert "NO_PROXY" in os.environ
+def test_setup():
+    # see .github/workflows/build.yml connectivity
+    # check that the env varibales are properly setup
+    assert os.getenv("HTTP_PROXY") == "http://127.0.0.1:8080"
+    assert os.getenv("HTTPS_PROXY") == "http://127.0.0.1:8080"
+    assert os.getenv("NO_PROXY") == "localhost,127.0.0.1"
+    # check that mitmproxy is running
+    s = socket.socket()
+    s.settimeout(2)
+    try:
+        s.connect(("127.0.0.1", "8080"))
+    finally:
+        s.close()
