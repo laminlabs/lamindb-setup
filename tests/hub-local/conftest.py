ad244a43cf7b5280e8dfc296e2f7711879ae91ae
Sergei Rybakov <koncopd@gmail.com>
2025-12-26 13:34:27 +0200

    ✅ Fix `hub-local` (#1248)
    * debug
    
    * rm LAMIN_API_KEY
    
    * reset user

--- tests/hub-local/conftest.py
+++ tests/hub-local/conftest.py
@@ -47,6 +47,9 @@ def pytest_configure():
     supabase_resources.reset_local()
     supabase_resources.migrate()
     seed_local_test()
+    # reset user
+    del os.environ["LAMIN_API_KEY"]
+    ln_setup.settings._user_settings = None
 
 
 def pytest_unconfigure():
@@ -79,7 +82,9 @@ def sign_up_user(email: str, handle: str, save_as_settings: bool = False):
 @pytest.fixture(scope="session")
 def create_testadmin1_session():  # -> Tuple[Client, UserSettings]
     email = "testadmin1@gmail.com"
-    sign_up_user(email, "testadmin1", save_as_settings=True)
+    usettings = sign_up_user(email, "testadmin1", save_as_settings=True)
+    assert usettings.handle == "testadmin1"
+
     with pytest.raises(AuthApiError):
         # test error with "User already registered"
         sign_up_user(email, "testadmin1")
