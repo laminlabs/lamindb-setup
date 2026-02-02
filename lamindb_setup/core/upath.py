9d31bc3f2349afb68090bcb37e2a636881d0c03c
Koncopd <koncopd@gmail.com>
2026-02-02 16:17:59 +0200

    fix progress for boto3 dl

--- lamindb_setup/core/upath.py
+++ lamindb_setup/core/upath.py
@@ -277,8 +277,8 @@ def print_hook(size: int, value: int, objectname: str, action: str):
         progress_in_percent = (value / size) * 100
     out = f"... {action} {objectname}: {min(progress_in_percent, 100):4.1f}%"
     if "NBPRJ_TEST_NBPATH" not in os.environ:
-        end = "\n" if progress_in_percent >= 100 else "\r"
-        print(out, end=end, flush=True)
+        end = "\n" if progress_in_percent >= 100 else ""
+        print("\r" + out, end=end, flush=True)
 
 
 class ProgressCallback(fsspec.callbacks.Callback):
