e7398db48a1874aa33086d31bb39869dfb33d3ba
Sergei Rybakov <koncopd@gmail.com>
2026-02-19 13:50:02 +0200

    🔒️ Scope aws credentials caches to users (#1299)
    Scope aws credentials caches to users

--- tests/hub-prod/test_aws_options_manager.py
+++ tests/hub-prod/test_aws_options_manager.py
@@ -1,9 +1,9 @@
-from lamindb_setup.core._aws_options import get_aws_options_manager
+from lamindb_setup.core._aws_options import get_user_aws_options_manager
 from lamindb_setup.core.upath import UPath
 
 
 def test_aws_options_manager():
-    aws_options_manager = get_aws_options_manager()
+    aws_options_manager = get_user_aws_options_manager()
     assert aws_options_manager.anon_public is not None
     assert not aws_options_manager.anon_public
 
