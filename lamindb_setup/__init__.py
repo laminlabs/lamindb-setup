bb12c9e5ffa8a7d621e94b697bd93b5d23f01e8c
Koncopd <koncopd@gmail.com>
2026-07-06 12:16:25 +0300

    🔖 Release 1.25.4

--- lamindb_setup/__init__.py
+++ lamindb_setup/__init__.py
@@ -35,7 +35,7 @@ Migration management
 
 """
 
-__version__ = "1.25.3"  # denote a release candidate for 0.1.0 with 0.1rc1
+__version__ = "1.25.4"  # denote a release candidate for 0.1.0 with 0.1rc1
 
 import os
 
