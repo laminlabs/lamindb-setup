9f5fd760162c03215e5885d51aa05af8adbb3dde
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-12 22:27:23 +0200

    🚸 Proper deprecation warning in legacy modules

--- lamindb_setup/core/types.py
+++ lamindb_setup/core/types.py
@@ -1,1 +1,9 @@
+import warnings
+
+warnings.warn(
+    "`lamindb_setup.core.types` is deprecated, use `lamindb_setup.types` instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 from lamindb_setup.types import AnyPathStr  # backward compatibility
