e3088a779763eba6e240723742496de1a846bd7d
Sergei Rybakov <koncopd@gmail.com>
2026-07-17 17:55:03 +0300

    ✅ Fix tests (#1365)
    fix

--- tests/hub-cloud/scripts/script-to-fail-managed-storage.py
+++ tests/hub-cloud/scripts/script-to-fail-managed-storage.py
@@ -2,21 +2,19 @@ from pathlib import Path
 
 import lamindb_setup as ln_setup
 import pytest
-from django.db.utils import ProgrammingError
 from lamindb_setup._set_managed_storage import set_managed_storage
 from lamindb_setup.core._hub_client import connect_hub_with_auth
+from postgrest.exceptions import APIError
 
 # a user should have read-only access to laminlabs/lamin-site-assets
 ln_setup.login("testuser1")
 ln_setup.connect("laminlabs/lamin-site-assets")
 
-from lamindb.errors import NoWriteAccess
-
 test_root = Path("./test_script_ci_storage").resolve().as_posix()
 
-with pytest.raises(NoWriteAccess) as error:
+with pytest.raises(APIError) as error:
     set_managed_storage(test_root, host="test-host-1234")
-assert "You're not allowed to write to the space 'all'" in str(error)
+assert 'new row violates row-level security policy for table "storage"' in str(error)
 
 hub_client = connect_hub_with_auth()
 
