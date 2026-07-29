b95b24b6ca76f4c92bbc0787cef3db8a2678a65e
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-29 13:52:28 +0200

    ♻️ Address Sergei's comments

--- lamindb_setup/core/upath.py
+++ lamindb_setup/core/upath.py
@@ -857,9 +857,9 @@ def from_auth(cls, path: AnyPathStr) -> UPath:
 
 def cache(self: UPath, cache_key: str | None = None, **kwargs) -> UPath:
     """Return a local cache path and synchronize for cloud paths."""
-    from ._settings import SetupPaths
+    from ._settings import settings
 
-    return SetupPaths.cloud_to_local(self, cache_key=cache_key, **kwargs)
+    return settings.paths.cloud_to_local(self, cache_key=cache_key, **kwargs)
 
 
 # Why aren't we subclassing?
@@ -885,7 +885,6 @@ UPath.from_auth = classmethod(from_auth)
 UPath.cache = cache
 # unfortunately, we also have to do this for the subclasses
 Path.view_tree = view_tree  # type: ignore
-Path.cache = cache  # type: ignore
 
 
 def _standardize_docstring_paragraphs(doc: str | None) -> str | None:
