923d59ea5b8b8dbc5d5e5ff4f415cb6e9cbf4a5c
Sheetal Giri <sheetal@lamin.ai>
2026-08-17 12:04:01 +0200

    Updated tests

--- tests/core/test_to_url.py
+++ tests/core/test_to_url.py
@@ -3,39 +3,39 @@ from __future__ import annotations
 from types import SimpleNamespace
 
 import lamindb_setup as ln_setup
+import pytest
 from lamindb_setup.core._settings import settings
 
 
-def test_to_url():
-    # us-east-1 / AWS Dev
-    # public bucket
-    assert (
-        ln_setup.core.upath.create_path("s3://lamindata/test-folder").to_url()
-        == "https://lamindata.s3.amazonaws.com/test-folder"
+def test_to_url_s3_public_us_east_1(monkeypatch):
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: True
     )
-    # private bucket
-    assert (
-        ln_setup.core.upath.create_path(
-            "s3://lamindb-setup-private-bucket/test-folder"
-        ).to_url()
-        == "https://lamindb-setup-private-bucket.s3.amazonaws.com/test-folder"
+    monkeypatch.setattr(
+        ln_setup.core.upath, "get_storage_region", lambda _: "us-east-1"
     )
-    # eu-central-1 / AWS Dev
+    upath = ln_setup.core.upath.UPath("s3://lamindata/test-folder")
+    assert upath.to_url() == "https://lamindata.s3.amazonaws.com/test-folder"
+
+
+def test_to_url_s3_public_regional(monkeypatch):
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: True
+    )
+    monkeypatch.setattr(
+        ln_setup.core.upath, "get_storage_region", lambda _: "eu-central-1"
+    )
+    upath = ln_setup.core.upath.UPath("s3://lamindata-eu/test-folder")
     assert (
-        ln_setup.core.upath.create_path("s3://lamindata-eu/test-folder").to_url()
+        upath.to_url()
         == "https://lamindata-eu.s3-eu-central-1.amazonaws.com/test-folder"
     )
-    # eu-central-1 / AWS Hosted
-    # below is the default storage of the lamin-dev instance
-    assert (
-        ln_setup.core.upath.create_path(
-            "s3://lamin-eu-central-1/9fm7UN13/test-folder"
-        ).to_url()
-        == "https://lamin-eu-central-1.s3-eu-central-1.amazonaws.com/9fm7UN13/test-folder"
+
+
+def test_to_url_gcs_root(monkeypatch):
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: True
     )
-
-
-def test_to_url_gcs_root():
     upath = ln_setup.core.upath.UPath(
         "gs://rxrx1-europe-west4/images/test/HEPG2-08/Plate1/B02_s1_w1.png"
     )
@@ -51,7 +51,9 @@ def test_to_url_https_root():
 
 
 def test_to_url_s3_hub_private_route(monkeypatch):
-    monkeypatch.setattr(ln_setup.core.upath, "_is_public_s3_path", lambda _: False)
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: False
+    )
     monkeypatch.setattr(
         settings,
         "_instance_settings",
@@ -66,7 +68,9 @@ def test_to_url_s3_hub_private_route(monkeypatch):
 
 
 def test_to_url_s3_public_stays_native(monkeypatch):
-    monkeypatch.setattr(ln_setup.core.upath, "_is_public_s3_path", lambda _: True)
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: True
+    )
     monkeypatch.setattr(
         ln_setup.core.upath, "get_storage_region", lambda _: "us-east-1"
     )
@@ -80,3 +84,32 @@ def test_to_url_s3_public_stays_native(monkeypatch):
     assert (
         upath.to_url() == "https://lamindb-ci.s3.amazonaws.com/test-data/test.parquet"
     )
+
+
+def test_to_url_s3_private_not_hub_raises(monkeypatch):
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: False
+    )
+    monkeypatch.setattr(
+        settings,
+        "_instance_settings",
+        SimpleNamespace(is_on_hub=False, ui_url=None),
+        raising=False,
+    )
+    upath = ln_setup.core.upath.UPath("s3://private-bucket/secret/file.csv")
+    with pytest.raises(
+        ValueError,
+        match="must be publicly accessible or the artifact must be hosted on LaminHub",
+    ):
+        upath.to_url()
+
+
+def test_to_url_gcs_private_raises(monkeypatch):
+    monkeypatch.setattr(
+        ln_setup.core.upath, "_is_publicly_accessible_path", lambda _: False
+    )
+    upath = ln_setup.core.upath.UPath(
+        "gs://rxrx1-europe-west4/images/test/HEPG2-08/Plate1/B02_s1_w1.png"
+    )
+    with pytest.raises(ValueError, match="only supports publicly accessible GCS paths"):
+        upath.to_url()
