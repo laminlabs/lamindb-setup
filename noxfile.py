be7c3359e05c83193976f7cb65fbb97ff8aad8c9
Koncopd <koncopd@gmail.com>
2026-08-21 20:44:16 +0300

    :green_heart: Fix CI

--- noxfile.py
+++ noxfile.py
@@ -27,7 +27,7 @@ def lint(session: nox.Session) -> None:
     ["hub-local", "hub-prod", "hub-cloud", "core", "connectivity", "docs"],
 )
 def install(session: nox.Session, group: str) -> None:
-    no_deps_packages = "git+https://github.com/laminlabs/pertdb git+https://github.com/laminlabs/lamin-cli@defaultstorage"
+    no_deps_packages = "git+https://github.com/laminlabs/pertdb git+https://github.com/laminlabs/lamin-cli"
     modules_deps = f"""uv pip install --system "git+https://github.com/laminlabs/lamindb[full]"
 uv pip install --system --no-deps {no_deps_packages}
 uv pip install --system git+https://github.com/laminlabs/bionty
