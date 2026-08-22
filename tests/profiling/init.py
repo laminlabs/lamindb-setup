a2fafcf64e7748e7ff4ac997272ba1ab9537d2e5
Alex Wolf <f.alexander.wolf@gmail.com>
2026-08-22 18:16:45 +0200

    💚 Fix

--- tests/profiling/init.py
+++ tests/profiling/init.py
@@ -1,3 +1,14 @@
-from lamindb_setup import init
+def main():
+    from lamindb_setup import init
 
-init()
+    init()
+
+
+def cleanup():
+    from lamindb_setup import delete
+
+    delete("lamindb-setup", force=True)
+
+
+if __name__ == "__main__":
+    main()
