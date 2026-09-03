7137a8bb14ac21152553630997a77e6bf254b46d
Koncopd <koncopd@gmail.com>
2026-09-03 14:08:52 +0300

    better handling

--- lamindb_setup/_setup_user.py
+++ lamindb_setup/_setup_user.py
@@ -130,6 +130,7 @@ def login(
         user_settings = UserSettings(handle="temporary", uid="null")
 
     from .core._hub_core import sign_in_hub, sign_in_hub_api_key
+    from .errors import ApiKeyExpired
 
     if api_key is None:
         response = sign_in_hub(
@@ -142,7 +143,7 @@ def login(
         user_settings.password = None
 
     if isinstance(response, Exception):
-        if "expired" in str(response).lower():
+        if isinstance(response, ApiKeyExpired):
             raise response from None
         raise response
     elif isinstance(response, str):
