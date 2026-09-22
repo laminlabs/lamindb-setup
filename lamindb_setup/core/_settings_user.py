9d1fc0a490b8bfeb2b73c899baaa0fcfdb981bde
Koncopd <koncopd@gmail.com>
2026-09-22 15:10:08 +0300

    refreshable token

--- lamindb_setup/core/_settings_user.py
+++ lamindb_setup/core/_settings_user.py
@@ -1,40 +1,100 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+import time
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from uuid import UUID
 
 
-class user_description:
-    email = """User email."""
-    password = """API key or legacy password."""
-    uid = """Universal user ID."""
-    handle = "Unique handle."
-    name = "Full name."
+class AccessToken:
+    def __init__(self, access_token: str, user_settings: UserSettings):
+        self._access_token: str = access_token
+        self._expiration: float = self._get_expiration(access_token)
+        # needed to refresh access token when it expires
+        self._user_settings: UserSettings = user_settings
+
+    @staticmethod
+    def _get_expiration(access_token: str) -> float:
+        from jwt import decode
+
+        # buffer time of 1 hour
+        return decode(access_token, options={"verify_signature": False})["exp"] - 3600
+
+    def _refresh_token(self):
+        if time.time() >= self._expiration:
+            from ._hub_client import get_access_token
+
+            new_access_token = get_access_token(
+                self._user_settings.email,
+                self._user_settings.password,
+                self._user_settings.api_key,
+            )
+            if new_access_token is not None:
+                self._access_token = new_access_token
+                self._expiration = self._get_expiration(new_access_token)
+
+    @property
+    def access_token(self) -> str:
+        self._refresh_token()
+        return self._access_token
 
 
-@dataclass
 class UserSettings:
     """User data. All synched from cloud."""
 
-    handle: str = "anonymous"
+    handle: str
     """Unique handle."""
-    email: str | None = None
+    email: str | None
     """User email."""
-    api_key: str | None = None
+    api_key: str | None
     """API key."""
-    password: str | None = None
+    password: str | None
     """Legacy password."""
-    access_token: str | None = None
+    access_token: str | None
     """User access token."""
-    uid: str = "null"
+    uid: str
     """Universal user ID."""
-    _uuid: UUID | None = None
+    _uuid: UUID | None
     """Lamin's internal user ID."""
-    name: str | None = None
+    name: str | None
     """Full name."""
+
+    def __init__(
+        self,
+        handle: str = "anonymous",
+        email: str | None = None,
+        api_key: str | None = None,
+        password: str | None = None,
+        access_token: str | None = None,
+        uid: str = "null",
+        _uuid: UUID | None = None,
+        name: str | None = None,
+    ):
+        self.handle = handle
+        self.email = email
+        self.api_key = api_key
+        self.password = password
+        self.uid = uid
+        self._uuid = _uuid
+        self.name = name
+        # passes through the setter
+        self.access_token = access_token
+
+    @property  # type: ignore[no-redef]
+    def access_token(self) -> str | None:
+        """User access token."""
+        refreshable_access_token = self._refreshable_access_token
+        if refreshable_access_token is not None:
+            return refreshable_access_token.access_token
+        return None
+
+    @access_token.setter
+    def access_token(self, value: str | None):
+        token: AccessToken | None = (
+            AccessToken(value, self) if value is not None else None
+        )
+        self._refreshable_access_token = token
 
     def __repr__(self) -> str:
         """Rich string representation."""
